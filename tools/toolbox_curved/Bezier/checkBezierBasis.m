ccf1e02fd4228fbb32119d2666680ff1c1e0ea98
iagcopple <iagcopple@f8cfc4a6-4e3a-43c3-8588-810d72ca601e>
2014-09-03 05:43:48 +0000

    update
    git-svn-id: svn+ssh://iagpc151.iag.uni-stuttgart.de/usr/local/svnroot/Boltzplatz/branches/PICASSO@359 f8cfc4a6-4e3a-43c3-8588-810d72ca601e

--- tools/toolbox_curved/Bezier/checkBezierBasis.m
+++ tools/toolbox_curved/Bezier/checkBezierBasis.m
@@ -1,10 +1,11 @@
 function plot_cases
 clc; clear all; close all;
-global Bezier2d;global n_Ls;global n_Ls_inv;global projectedFace;global Face;
+global Bezier2d;global n_Ls;global n_Ls_inv;global projectedFace;global Face;global BezierSurface
 NGeo=2;
 
 load faces.dat
-load /home/stephen/PMLalgorithm_cases/Testing_Bezier_domain010_nElems002_order04_PML00_zeta0E+00_polynom_CFL0.5_N10_Parts500_DoPML_False/BezierControlPoints.dat
+%load /home/stephen/PMLalgorithm_cases/Testing_Bezier_domain010_nElems002_order04_PML00_zeta0E+00_polynom_CFL0.5_N10_Parts500_DoPML_False/BezierControlPoints.dat
+load BezierControlPoints.dat
 %factorial(5)
 faces=BezierControlPoints;
 dimension = size(faces);
@@ -25,7 +26,20 @@ for i=1:nSides
     end
   end
 end
-figure;  hold on;set(gcf, 'color', 'white');view(29,60);grid on;xlabel('x');ylabel('y');zlabel('z');
+%%
+
+
+Face=10;
+pvec=[0.4 0.2 0.001];
+v=[0 1 0];Face=9;
+v=[0.7 0.9 1];Face=9;
+%v=[1 0 0]
+%v=[0 0 1];Face=10;
+%v=[-1 1 0]
+pvec=[0.3 0.3 0.001];v=[1 1 0];Face=9;
+%%
+
+BezierSurface=figure;  hold on;set(gcf, 'color', 'white');view(29,60);grid on;xlabel('x');ylabel('y');zlabel('z');
 %plot3(faces(:,1),faces(:,2),faces(:,3),'ro','MarkerSize',10,'LineWidth',5)
 Xi_NGeo=zeros(1,N+1);
 for I=1:N+1
@@ -39,7 +53,7 @@ for i=1:nSides
     index1=(i-1)*(N+1)^2+1;
     index2=i*(N+1)^2;
     %plot3(faces(index1:index2,1),faces(index1:index2,2),faces(index1:index2,3),'ro','MarkerSize',10,'LineWidth',5)
-    g_h(N,P(:,:,:,i));
+    g_h(N,P(:,:,:,i),i);
     count=count+1;
     index=i;
     %return
@@ -63,13 +77,6 @@ SideID=9;dimensions=size(P);
 BezierControlPoints=P;
 BezierControlPoints(:,:,:,SideID);
 
-Face=10;
-pvec=[0.2 0.2 0.001];
-v=[0 1 0];
-v=[0.7 0.9 1];
-%v=[1 0 0]
-v=[0 0 1]
-%v=[-1 1 0]
 v=1/(v*v')*v;
 p2=pvec+0.3*v;
 plot3(pvec(1),pvec(2),pvec(3),'or','LineWidth',4,'MarkerSize',10)
@@ -138,28 +145,28 @@ BezierControlPoints1D=zeros(dimensions(1),dimensions(2),dimensions(4));
 xi=linspace(-1,1,NGeo+1);eta=linspace(-1,1,NGeo+1);
 minmax=zeros(2,NGeo+1)-2;
 %disp(['n_Ls_inv*n_Ls=' num2str(n_Ls_inv*n_Ls') ])
-plot([0 n_Ls_inv(1)*0.25], [0 n_Ls_inv(2)*0.25],'k-','LineWidth',2,'MarkerSize',10)
+
 boundaries=[-1 1; -1 1];
 DoClipping=ones(2,1);
-for K=1:10
+for K=1:15
   for J=1:2
     if true(DoClipping(J))
       switch J
         case 1
           vector = [n_Ls_inv(1) n_Ls_inv(2)];posX=pos3;
         case 2
-          vector = n_Ls;posX=pos4;
+          vector = [n_Ls(1) n_Ls(2)];posX=pos4;
       end
       f=figure;hold on;set(gcf, 'color', 'white');grid on;xlabel('x');ylabel('y');plot([-1 1],[0 0],'k-');set(gcf,'OuterPosition',posX)
       for SideID=1:nSides
         for q=1:NGeo+1
           for p=1:NGeo+1
-            BezierControlPoints1D(p,q,SideID)=[BezierControlPoints2D(p,q,1,SideID) BezierControlPoints2D(p,q,2,SideID)]*vector';
+            BezierControlPoints1D(p,q,SideID)=([BezierControlPoints2D(p,q,1,SideID) BezierControlPoints2D(p,q,2,SideID)])*vector';
           end
         end
       end
       for I=1:NGeo+1
-        plot(xi,BezierControlPoints1D(:,I,9),'ko-')
+        plot(xi,BezierControlPoints1D(:,I,Face),'ko-')
         minmax(2,I)=max(BezierControlPoints1D(I,:,Face)); % Upper
         minmax(1,I)=min(BezierControlPoints1D(I,:,Face)); % Lower
       end
@@ -168,34 +175,34 @@ for K=1:10
       smax=-1.5;
       %check streckenzug upper/lower
       for I=1:NGeo
-        if  minmax(2,I)*minmax(2,I+1)<0 % Upper
+        if  minmax(2,I)*minmax(2,I+1)<=0 % Upper
           m=(minmax(2,I+1)-minmax(2,I))/(xi(I+1)-xi(I));
           smin_proposed=xi(I)-minmax(2,I)/m;
           smin=min(smin_proposed,smin);
         end
-        if  minmax(1,I)*minmax(1,I+1)<0 % Upper
+        if  minmax(1,I)*minmax(1,I+1)<=0 % Upper
           m=(minmax(1,I+1)-minmax(1,I))/(xi(I+1)-xi(I));
           smax_proposed=xi(I)-minmax(1,I)/m;
           smax=max(smax_proposed,smax);
         end
       end
       %% streckenzug:
-      if  minmax(2,1)*minmax(2,NGeo+1)<0 %1. zeile anfang/ende -> upper
+      if  minmax(2,1)*minmax(2,NGeo+1)<=0 %1. zeile anfang/ende -> upper
         m=(minmax(2,NGeo+1)-minmax(2,1))/(xi(NGeo+1)-xi(1));
         smin_proposed=xi(1)-minmax(2,1)/m;
         smin=min(smin_proposed,smin);
       end
-      if  minmax(1,1)*minmax(1,NGeo+1)<0 %2. zeile anfang/ende -> lower
+      if  minmax(1,1)*minmax(1,NGeo+1)<=0 %2. zeile anfang/ende -> lower
         m=(minmax(1,NGeo+1)-minmax(1,1))/(xi(NGeo+1)-xi(1));
         smax_proposed=xi(1)-minmax(1,1)/m;
         smax=max(smax_proposed,smax);
       end
       %% streckenzug:
-      if  minmax(1,1)*minmax(2,1)<0 % 1 spalte anfang/ende
+      if  minmax(1,1)*minmax(2,1)<=0 % 1 spalte anfang/ende
         smin_proposed=xi(1);
         smin=min(smin_proposed,smin);
       end
-      if  minmax(1,2)*minmax(2,2)<0 % 2 spalte anfang/ende
+      if  minmax(1,2)*minmax(2,2)<=0 % 2 spalte anfang/ende
         smax_proposed=xi(NGeo+1);
         smax=max(smax_proposed,smax);
       end
@@ -207,31 +214,54 @@ for K=1:10
       %%
       boundaries(J,:)=[smin smax];
       %% CLIP
-%       if J==2
-%         smax=0.5
-%         smin=-0.5
-%       end
-      disp(['K=' num2str(K) ' J=' num2str(J) ' DoClipping(J)=' num2str(DoClipping(J)) ' smax-smin=' num2str(smax-smin) ' (' num2str((smax-smin)/2*100) '%)'])
-      if (smax-smin)/2*100>40,disp('gebiet muss geteilt werden!');end
+      %       if J==2
+      %         smax=0.5
+      %         smin=-0.5
+      %       end
+      x_sol=mean(mean(BezierControlPoints2D(:,:,1,Face)));
+      y_sol=mean(mean(BezierControlPoints2D(:,:,2,Face)));
+      switch J
+        case 1
+          direction='u';
+        case 2
+          direction='v';
+      end
+      disp(['K=' num2str(K) ' J=' num2str(J) ' Clip ' direction '  smin=' sprintf('%1.2e',smin) ', smax=' sprintf('%1.2e',smax) ' smax-smin=' sprintf('%1.2e',smax-smin) ' (' sprintf('%3.2f',(smax-smin)/2*100) '%) delta=' sprintf('%1.2e',sqrt(x_sol^2+y_sol^2))])
+      %if (smax-smin)/2*100>40,disp('gebiet muss geteilt werden!');end
       BezierControlPoints2D(:,:,:,Face)=BezierClipping(J,smin,smax,N,BezierControlPoints2D(:,:,:,Face),2); % J={u,v}
       %% check epsilon
-
+      
       if abs(smax-smin)<1e-4
         DoClipping(J)=0;
       end
-      str=input('Press ENTER to continue or type "y" to exit: ','s');
-      if strcmp(str,'y'),error('You have terminated the program!');end;
+      %str=input('Press ENTER to continue or type "y" to exit: ','s');
+      %if strcmp(str,'y'),error('You have terminated the program!');end;
 
+      if sqrt(x_sol^2+y_sol^2)<1E-4
+        disp(['sqrt(x_sol^2+y_sol^2)<1E-4: origin found!'])
+        plot(x_sol,y_sol, 'g+','Parent',get(projectedFace, 'children'),'MarkerSize',20,'LineWidth',2)
+        plot(x_sol,y_sol, 'go','Parent',get(projectedFace, 'children'),'MarkerSize',20,'LineWidth',2)
+        return
+      end
     end
+    
   end
-  
+  disp(' ')
+  calcLsLines(BezierControlPoints2D(:,:,:,Face))
 end
+plot(x_sol,y_sol, 'r+','Parent',get(projectedFace, 'children'),'MarkerSize',20,'LineWidth',2)
+plot(x_sol,y_sol, 'ro','Parent',get(projectedFace, 'children'),'MarkerSize',20,'LineWidth',2)
+plot_2D_clipped_Bezier(N,BezierControlPoints2D(:,:,:,Face),[-1 1; -1 1])
+
+
+
+
+
 return
 boundaries(:,1)=boundaries(:,1)-1E-4;
 boundaries(:,2)=boundaries(:,2)+1E-4;
 
-g_h2D_clipped(N,BezierControlPoints2D(:,:,:,Face),boundaries);
-
+plot_2D_clipped_Bezier(N,BezierControlPoints2D(:,:,:,Face),boundaries);
 
 
 %%
@@ -248,7 +278,8 @@ end
 
 
 
-function g_h=g_h(N,P)
+function g_h=g_h(N,P,SideID) % 3D
+global ParticleBezierSurface;global Face
 N_plot=5;
 xi=linspace(-1,1,N_plot);eta=linspace(-1,1,N_plot);
 Bezier=zeros(length(xi),length(eta),3);
@@ -260,18 +291,24 @@ for I=1:length(xi)
         %disp(['p=' num2str(p) ' q=' num2str(q) ])
         %disp(['n=' num2str(N) ' k=' num2str(p-1) '(p)'])
         % disp(['n=' num2str(N) ' k=' num2str(q-1) '(q)'])
-        plot3(P(p,q,1),P(p,q,2),P(p,q,3),'go','MarkerSize',10,'LineWidth',5)
+        plot3(P(p,q,1),P(p,q,2),P(p,q,3),'bo','MarkerSize',10,'LineWidth',2)
         Bezier(I,J,:)=Bezier(I,J,:)+B(N,p-1,xi(I))*B(N,q-1,eta(J))*P(p,q,:);
       end
     end
   end
 end
 %Bezier
-h=surf(Bezier(:,:,1),Bezier(:,:,2),Bezier(:,:,3));
-set(h,'FaceAlpha',0.8)
+if SideID==Face
+  ParticleBezierSurface=surf(Bezier(:,:,1),Bezier(:,:,2),Bezier(:,:,3));
+  set(ParticleBezierSurface,'FaceAlpha',0.9)
+else
+  h=surf(Bezier(:,:,1),Bezier(:,:,2),Bezier(:,:,3));
+  set(h,'FaceAlpha',0.25)
 end
 
-function g_h2D=g_h2D(N,P)
+end
+
+function g_h2D=g_h2D(N,P) % 2D
 global Bezier2d
 global n_Ls
 global n_Ls_inv
@@ -287,7 +324,7 @@ for I=1:length(xi)
   for J=1:length(eta)
     for q=1:N+1
       for p=1:N+1
-        plot(P(p,q,1),P(p,q,2),'go','MarkerSize',10,'LineWidth',5)
+        plot(P(p,q,1),P(p,q,2),'bo','MarkerSize',10,'LineWidth',3)
         Bezier2d(I,J,:)=Bezier2d(I,J,:)+B(N,p-1,xi(I))*B(N,q-1,eta(J))*P(p,q,:);
       end
     end
@@ -297,52 +334,54 @@ plot(0,0,'ko','MarkerSize',10,'LineWidth',5)
 plot(Bezier2d(:,:,1),Bezier2d(:,:,2),'b-',Bezier2d(:,:,1)',Bezier2d(:,:,2)','b-');
 
 %% get Line L_s (vector n_Ls and n_Ls_inv)
-if 1==1
-  plot([P(1,1,1) P(1,end,1)],[P(1,1,2) P(1,end,2)],'LineWidth',5)
-  plot([P(end,1,1) P(end,end,1)],[P(end,1,2) P(end,end,2)],'LineWidth',5)
-  dx=(P(1,end,1)-P(1,1,1)+P(end,end,1)-P(end,1,1));
-  if abs(dx)>1E-6
-    dy=(P(1,end,2)-P(1,1,2)+P(end,end,2)-P(end,1,2))
-    m=dy/dx;
-    n_Ls=[dx dy];
-    n_Ls=1/(n_Ls*n_Ls')*n_Ls;
-    if abs(dy)>1E-6
-      n_Ls_inv=[-dy dx];
-    else
-      n_Ls_inv=[0 1];
-    end
-  else
-    n_Ls=[0 1];
-    n_Ls_inv=[1 0];
-    m=111110;
-  end
-  x=linspace(-0.2,0.2,2);
-  plot(x,m*x,'k-','LineWidth',2)
-  axis equal
-  return
-  %neues n_Ls_inv, welches nicht senkrecht auf dem anderen vektor steht
- % plot(P(1,1,1),P(1,1,2),'+r')
- % plot(P(end,1,1),P(end,1,2),'or')
-  u1=P(end,1,:)-P(1,1,:);
-  u2=P(end,end,:)-P(1,end,:);
-  n_Ls_inv=u1+u2
-  n_Ls_inv=(1/sqrt([n_Ls_inv(1) n_Ls_inv(2)]*[n_Ls_inv(1);n_Ls_inv(2)]))*n_Ls_inv
-else
-  
-end
+% if 1==2
+%   plot([P(1,1,1) P(1,end,1)],[P(1,1,2) P(1,end,2)],'LineWidth',5)
+%   plot([P(end,1,1) P(end,end,1)],[P(end,1,2) P(end,end,2)],'LineWidth',5)
+%   dx=(P(1,end,1)-P(1,1,1)+P(end,end,1)-P(end,1,1));
+%   if abs(dx)>1E-6
+%     dy=(P(1,end,2)-P(1,1,2)+P(end,end,2)-P(end,1,2))
+%     m=dy/dx;
+%     n_Ls=[dx dy];
+%     n_Ls=1/(n_Ls*n_Ls')*n_Ls;
+%     if abs(dy)>1E-6
+%       n_Ls_inv=[-dy dx];
+%     else
+%       n_Ls_inv=[0 1];
+%     end
+%   else
+%     n_Ls=[0 1];
+%     n_Ls_inv=[1 0];
+%     m=111110;
+%   end
+%   x=linspace(-0.2,0.2,2);
+%   plot(x,m*x,'k-','LineWidth',2)
+axis equal
+%
+%   %neues n_Ls_inv, welches nicht senkrecht auf dem anderen vektor steht
+%   % plot(P(1,1,1),P(1,1,2),'+r')
+%   % plot(P(end,1,1),P(end,1,2),'or')
+%   u1=P(end,1,:)-P(1,1,:);
+%   u2=P(end,end,:)-P(1,end,:);
+%   n_Ls_inv=u1+u2
+%   n_Ls_inv=(1/sqrt([n_Ls_inv(1) n_Ls_inv(2)]*[n_Ls_inv(1);n_Ls_inv(2)]))*n_Ls_inv
+%   return
+% else
+%
+calcLsLines(P)
+% end
 
 
 %testing
-n_Ls_inv(:,:,1)=1
-n_Ls_inv(:,:,2)=1
-n_Ls_inv=(1/sqrt([n_Ls_inv(1) n_Ls_inv(2)]*[n_Ls_inv(1);n_Ls_inv(2)]))*n_Ls_inv
-
-    n_Ls=[1 -1];
-    n_Ls=1/(n_Ls*n_Ls')*n_Ls;
+% n_Ls_inv(:,:,1)=1
+% n_Ls_inv(:,:,2)=1
+% n_Ls_inv=(1/sqrt([n_Ls_inv(1) n_Ls_inv(2)]*[n_Ls_inv(1);n_Ls_inv(2)]))*n_Ls_inv
+%
+% n_Ls=[1 -1];
+% n_Ls=1/(n_Ls*n_Ls')*n_Ls;
 
 end
 
-function g_h2D_clipped=g_h2D_clipped(N,P,boundaries)
+function plot_2D_clipped_Bezier(N,P,boundaries) % plot clipped bezier curve (without new control points)
 global Bezier2d;global n_Ls;global n_Ls_inv;global projectedFace
 N_plot=5;
 xi=linspace(boundaries(1,1),boundaries(1,2),N_plot)
@@ -363,29 +402,7 @@ for I=1:length(xi)
     end
   end
 end
-% plot(0,0,'ko','MarkerSize',10,'LineWidth',5)
 plot(Bezier2d(:,:,1),Bezier2d(:,:,2),'r-',Bezier2d(:,:,1)',Bezier2d(:,:,2)','r-','Parent',get(projectedFace, 'children'));
-% plot([P(1,1,1) P(1,end,1)],[P(1,1,2) P(1,end,2)],'LineWidth',5)
-% plot([P(end,1,1) P(end,end,1)],[P(end,1,2) P(end,end,2)],'LineWidth',5)
-% dx=(P(1,end,1)-P(1,1,1)+P(end,end,1)-P(end,1,1));
-% if abs(dx)>1E-6
-%   dy=(P(1,end,2)-P(1,1,2)+P(end,end,2)-P(end,1,2))
-%   m=dy/dx;
-%   n_Ls=[dx dy];
-%   n_Ls=1/(n_Ls*n_Ls')*n_Ls;
-%   if abs(dy)>1E-6
-%     n_Ls_inv=[-dy dx];
-%   else
-%     n_Ls_inv=[0 1];
-%   end
-% else
-%   n_Ls=[0 1];
-%   n_Ls_inv=[1 0];
-%   m=111110;
-% end
-% x=linspace(-0.2,0.2,2);
-% plot(x,m*x,'k-','LineWidth',2)
-% axis equal
 end
 
 function B=B(n,k,x)
@@ -453,10 +470,10 @@ switch method
         %axes1 = axes('Parent',projectedFace)
         plot(BezierControlPoints2D_temp(:,:,1),BezierControlPoints2D_temp(:,:,2),'bo','Parent',get(projectedFace, 'children'))
       end
-      result(L,:)=BezierControlPoints2D_temp
+      result(L,:)=BezierControlPoints2D_temp;
       plot(result(L,1),result(L,2),'sr-','Parent',get(projectedFace, 'children'))
     end
-    patch=[result;result(1,:)]
+    patch=[result;result(1,:)];
     plot(patch(:,1),patch(:,2),'r-','Parent',get(projectedFace, 'children'))
     
   case 2
@@ -464,20 +481,22 @@ switch method
     
     switch direction
       case 1 %u space
-        disp('u space')
-%         umin=smin;umax=smax;
-%         vmin=-1;  vmax=1;
+        %disp('u space')
+        %         umin=smin;umax=smax;
+        %         vmin=-1;  vmax=1;
+        color='k+';
       case 2 %v space
-        disp('v space')
+        %disp('v space')
         BezierControlPoints2D(:,:,1)=transpose(BezierControlPoints2D(:,:,1));
         BezierControlPoints2D(:,:,2)=transpose(BezierControlPoints2D(:,:,2));
-%         vmin=smin;vmax=smax;
-%         umin=-1;  umax=1;;
+        %         vmin=smin;vmax=smax;
+        %         umin=-1;  umax=1;;
+        color='rs';
     end
     
     Bezier2d=zeros(N+1,N+1,2);
     Bezier2d_temp=BezierControlPoints2D;
-    plot(BezierControlPoints2D(:,:,1),BezierControlPoints2D(:,:,2),'ro',BezierControlPoints2D(:,:,1)',BezierControlPoints2D(:,:,2)','ro','Parent',get(projectedFace, 'children'));
+    %plot(BezierControlPoints2D(:,:,1),BezierControlPoints2D(:,:,2),'ro',BezierControlPoints2D(:,:,1)',BezierControlPoints2D(:,:,2)','ro','Parent',get(projectedFace, 'children'));
     %% 1. direction
     s=smax;
     for J=1:N+1
@@ -488,7 +507,7 @@ switch method
         end
       end
     end
-    plot(Bezier2d(:,:,1),Bezier2d(:,:,2),'rs',Bezier2d(:,:,1)',Bezier2d(:,:,2)','r+','Parent',get(projectedFace, 'children'),'LineWidth',3);
+    %plot(Bezier2d(:,:,1),Bezier2d(:,:,2),'ks',Bezier2d(:,:,1)',Bezier2d(:,:,2)','r+','Parent',get(projectedFace, 'children'));
     %% 2. direction
     %  Bezier2d_temp(:,:,1)=transpose(Bezier2d(:,:,1));% altes array mit neuen punkten überschreiben für 2. clip
     %   Bezier2d_temp(:,:,2)=transpose(Bezier2d(:,:,2));
@@ -505,9 +524,30 @@ switch method
         end
       end
     end
-    plot(Bezier2d(:,:,1),Bezier2d(:,:,2),'g^',Bezier2d(:,:,1)',Bezier2d(:,:,2)','g+','Parent',get(projectedFace, 'children'),'LineWidth',3);
+    plot(Bezier2d(:,:,1),Bezier2d(:,:,2),color,Bezier2d(:,:,1)',Bezier2d(:,:,2)',color,'Parent',get(projectedFace, 'children'));
     BezierClipping=Bezier2d;
     %BezierClipping=new points
     
 end
+
+end
+
+
+function  calcLsLines(P)
+global n_Ls
+global n_Ls_inv
+global projectedFace
+%% 1st direction
+v1=P(1,end,:)-P(1,1,:);
+v2=P(end,end,:)-P(end,1,:);
+n_Ls=v1+v2;
+n_Ls=(1/sqrt([n_Ls(1) n_Ls(2)]*[n_Ls(1);n_Ls(2)]))*n_Ls;
+%% 2nd direction
+u1=P(end,1,:)-P(1,1,:);
+u2=P(end,end,:)-P(1,end,:);
+n_Ls_inv=u1+u2;
+n_Ls_inv=(1/sqrt([n_Ls_inv(1) n_Ls_inv(2)]*[n_Ls_inv(1);n_Ls_inv(2)]))*n_Ls_inv;
+%% plot result
+plot([0 n_Ls(1)*0.25], [0 n_Ls(2)*0.25],'k-','LineWidth',2,'MarkerSize',10,'Parent',get(projectedFace, 'children'))
+plot([0 n_Ls_inv(1)*0.25], [0 n_Ls_inv(2)*0.25],'k-','LineWidth',2,'MarkerSize',10,'Parent',get(projectedFace, 'children'))
 end
