6e6c7bbca9fb40ef13781c45cfbda8f5e51ae3f0
iagortwe <iagortwe@f8cfc4a6-4e3a-43c3-8588-810d72ca601e>
2015-06-18 18:42:08 +0000

    und preprocflags raus, wird immer gebaut
    git-svn-id: svn+ssh://iagpc151.iag.uni-stuttgart.de/usr/local/svnroot/Boltzplatz/branches/PICASSO@736 f8cfc4a6-4e3a-43c3-8588-810d72ca601e

--- src/preprocflags.f90
+++
@@ -1,8 +0,0 @@
-MODULE MOD_PreProcFlags
-IMPLICIT NONE
-CHARACTER(LEN=1000) :: PREPROC_FLAGS = &
-"mpif90 -xf95-cpp-input -cpp -fdefault-real-8 -fdefault-double-8 -fbackslash -DGNU -Wall -g -O0 -ggdb3 -fbounds-check -f"//&
-"init-real=nan -fbacktrace -fsanitize=undefined -fno-omit-frame-pointer -DPP_TimeDiscMethod=2 -DPP_NodeType=1 -DPP_Rieman"//&
-"n=1 -DPP_nVar=8 -DPP_nElems=NELEMZ -DPP_N=N -DPARTICLES=T -DMPI -DGNU -I. -I../share/GNU-MPI/hdf5-1.8.14/hdf5/include/ ""//&
-"
-END MODULE MOD_PreProcFlags
