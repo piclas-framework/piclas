abba91555107810a6223dda11ee93956fc8f77b4
iagortwe <iagortwe@f8cfc4a6-4e3a-43c3-8588-810d72ca601e>
2015-12-17 08:46:51 +0000

    Added paraview tools: HO-Grid Filter, Coord-Trafo und diff 2 datafiles
    git-svn-id: svn+ssh://iagpc151.iag.uni-stuttgart.de/usr/local/svnroot/Boltzplatz/branches/PICASSO@952 f8cfc4a6-4e3a-43c3-8588-810d72ca601e

---
+++ tools/paraview/coordtrafo/coordtrafo.py
@@ -0,0 +1,85 @@
+# Programmable Filter for computing the particle distribution
+
+Name = 'Coordinate Trafo'
+Label = 'Coordinate Trafo'
+Help = 'Computes vectors in cylindrical or spherical coordinates'
+
+NumberOfInputs = 1
+#OutputDataType = 'vtkPointsData'
+ExtraXml = ''
+
+Properties = dict(
+  WhichField=0,
+  Trafo=0
+  )
+
+def RequestData():
+  import math
+  import numpy
+  import paraview
+  import vtk.numpy_interface.dataset_adapter
+  import vtk.numpy_interface.algorithms
+  # -- this will import vtkMultiProcessController and vtkMPI4PyCommunicator
+  pdi = self.GetInput()
+  pdo = self.GetOutput()
+  numPoints = pdi.GetNumberOfPoints()
+  newField=vtk.vtkDoubleArray()
+  if WhichField == 0:
+      VarName    = "ElectricField"
+  elif WhichField == 1:
+      VarName    = "MagneticField"
+  elif WhichField == 2:
+      VarName    = "Velocity"
+  newField.SetNumberOfComponents(3)
+  if Trafo == 0:
+      # cylindrical coord
+      VarNameOut=''.join([VarName,'_CC'])
+      newField.SetComponentName(0,'r')
+      newField.SetComponentName(1,'theta')
+      newField.SetComponentName(2,'z')
+  else:
+      # spherical coord
+      VarNameOut=''.join([VarName,'_SC'])
+      newField.SetComponentName(0,'r')
+      newField.SetComponentName(1,'theta')
+      newField.SetComponentName(2,'phi')
+  newField.SetName(VarNameOut)
+  newField.SetNumberOfTuples(numPoints)
+  pdo.GetPointData().AddArray(newField)
+  if Trafo == 0:
+      # computes trafo in cylindrical coordinates
+      for i in range(0, numPoints):
+          coord = pdi.GetPoint(i)
+          x, y, z  = coord[:3]
+          FieldX = pdi.GetPointData().GetArray(VarName).GetValue(3*i  )
+          FieldY = pdi.GetPointData().GetArray(VarName).GetValue(3*i+1)
+          FieldZ = pdi.GetPointData().GetArray(VarName).GetValue(3*i+2)
+          theta=math.atan2(y,x)
+          FieldTheta=-FieldX*math.sin(theta)+FieldY*math.cos(theta)
+          FieldR    = FieldX*math.cos(theta)+FieldY*math.sin(theta)
+          #newData.InsertNextValue(theta)
+          newField.SetValue(3*i  ,FieldR)
+          newField.SetValue(3*i+1,FieldTheta)
+          newField.SetValue(3*i+2,FieldZ)
+  else:
+      # spherical coord 
+      for i in range(0, numPoints):
+          coord = pdi.GetPoint(i)
+          x, y, z  = coord[:3]
+          FieldX = pdi.GetPointData().GetArray(VarName).GetValue(3*i  )
+          FieldY = pdi.GetPointData().GetArray(VarName).GetValue(3*i+1)
+          FieldZ = pdi.GetPointData().GetArray(VarName).GetValue(3*i+2)
+          rho=math.sqrt(x*x+y*y+z*z)
+          theta=math.acos(z/rho)
+          phi=math.atan2(y,x)
+          FieldRho=math.sin(theta)*math.cos(phi)*FieldX+math.sin(theta)*math.sin(phi)*FieldY+math.cos(theta)*FieldZ
+          FieldTheta=math.cos(theta)*math.cos(phi)*FieldX+math.cos(theta)*math.sin(phi)*FieldY-math.sin(theta)*FieldZ
+          FieldPhi=-math.sin(phi)*FieldX+math.cos(phi)*FieldY
+          #newData.InsertNextValue(theta)
+          newField.SetValue(3*i  ,FieldRho)
+          newField.SetValue(3*i+1,FieldTheta)
+          newField.SetValue(3*i+2,FieldPhi)
+
+def RequestInformation():
+  from paraview import util
+  pdi = self.GetInput()
