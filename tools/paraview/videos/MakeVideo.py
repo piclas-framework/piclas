58cd4d5018ce56774dff27c68b8f9caeb7754ee4
iagcopple <iagcopple@f8cfc4a6-4e3a-43c3-8588-810d72ca601e>
2016-01-25 13:34:53 +0000

    update
    git-svn-id: svn+ssh://iagpc151.iag.uni-stuttgart.de/usr/local/svnroot/Boltzplatz/branches/PICASSO@1025 f8cfc4a6-4e3a-43c3-8588-810d72ca601e

--- tools/paraview/videos/MakeVideo.py
+++ tools/paraview/videos/MakeVideo.py
@@ -1,10 +1,14 @@
 RenderView1 = GetRenderView()
-RenderView1.CameraParallelScale = 0.5
+print RenderView1.CameraParallelScale
+#RenderView1.CameraParallelScale = 0.5
+#RenderView1.CameraParallelScale = 0.0001 # small means zoom in !
+#print RenderView1.CameraParallelScale
 Render()
 
 from paraview.simple import *
 import os
 import glob
+import sys
 from paraview import simple
 view = GetActiveView()
 [width,height]= view.ViewSize
@@ -17,35 +21,52 @@ if width%2==1:
 view.ViewSize = [width, height]
 print view.ViewSize
 # [1031, 687] Standard Settings for Samsung E172 Laptop
+view.CameraParallelScale=RenderView1.CameraParallelScale
+print view.CameraParallelScale
 
+# get active pipeline object (i.e. the statefile which has to be selected by hand)
+#S = GetActiveSource()
+#print S
+#P = S.FileName
+#Spath,Sfile = os.path.split(P)
+#os.chdir(Spath)
 
-S = GetActiveSource()
-print S
-P = S.FileName
-Spath,Sfile = os.path.split(P)
-os.chdir(Spath)
-
-pwd=os.getcwd() 
+pwd=os.getcwd() # get current path
 print pwd
 
+PSources=GetSources() # search through existing pipeline objects
+#sys.exit() # stop and close paraview
+#print PSources
+
+# get all state files in current folder
 List = glob.glob(pwd+"/*State*.h5")
 List = sorted(List)
 #print List
 K=0
 for file in List:
-    K=K+1
+    K=K+1 # count all state files
 N=0
 for file in List:
     Fpath,Ffile = os.path.split(file)    
     N=N+1
     print N," of ",K,"  (",Ffile,")"
-    TE23 = GetActiveSource()
-    TE23.FileName = file # das State file muss ausgewählt sein
+
+    # old
+    #TE23 = GetActiveSource()
+    #TE23.FileName = file # das State file muss ausgewählt sein
+    # new
+    for object in PSources:
+        # check all objects in pipiline and find out which are State files
+        if hasattr(PSources[object], 'FileName'):
+          if (PSources[object].FileName.find("State")>0.0 and 
+              PSources[object].FileName.find(".h5") > 0.0):
+              PSources[object].FileName=file
+    
     SetActiveView(view)
     Render()
-    simple.WriteImage("new"+"%0.3d" % N+".bmp")
+    simple.WriteImage("new"+"%0.4d" % N+".bmp")
     #N=N-1
 
 print "Creating Video ..."
-os.system('avconv -r 10 -y -i new%3d.bmp -b:v 4000k output.mp4')
+os.system('avconv -r 10 -y -i new%4d.bmp -b:v 4000k output.mp4')
 print "Done."
