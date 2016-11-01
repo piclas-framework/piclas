4012c1a2bb5935022ecbf2c76db7120394d9c843
Wladimir Reschke <reschke@irs.uni-stuttgart.de>
2016-11-01 16:46:18 +0100

    forgot allocate

--- src/mesh/mesh.f90
+++ src/mesh/mesh.f90
@@ -226,6 +226,7 @@ AnalyzeSide = 0
 !NOTE: nMortarSides=nMortarInnerSides+nMortarMPISides
 ALLOCATE(MortarType(2,1:nSides))              ! 1: Type, 2: Index in MortarInfo
 ALLOCATE(MortarInfo(MI_FLIP,4,nMortarSides)) ! [1]: 1: Neighbour sides, 2: Flip, [2]: small sides
+ALLOCATE(MortarSlave2MasterInfo(1:nSides))
 MortarType=-1
 MortarInfo=-1
 
