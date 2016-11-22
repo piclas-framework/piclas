653db6c60b0d2e60edb9c21dd4b92f5bcff684a3
Philip Ortwein <ortwein@iag.uni-stuttgart.de>
2016-11-22 17:51:45 +0100

    interpolate xcl_ngeo and dxcl_ngeo from tree for particle tracking.

--- src/mesh/metrics.f90
+++ src/mesh/metrics.f90
@@ -138,8 +138,10 @@ REAL    :: Vdm_CLN_N(         0:PP_N   ,0:PP_N)
 ! 3D Vandermonde matrices and lengths,nodes,weights
 REAL,DIMENSION(0:NgeoRef,0:NgeoRef) :: Vdm_xi_Ref,Vdm_eta_Ref,Vdm_zeta_Ref
 REAL,DIMENSION(0:PP_N   ,0:PP_N)    :: Vdm_xi_N  ,Vdm_eta_N  ,Vdm_zeta_N
+REAL,DIMENSION(0:NGeo   ,0:NGeo)    :: Vdm_xi_NGeo  ,Vdm_eta_NGeo  ,Vdm_zeta_NGeo
 REAL    :: xiRef( 0:NgeoRef),wBaryRef( 0:NgeoRef)
 REAL    :: xiCL_N(0:PP_N)   ,wBaryCL_N(0:PP_N)
+REAL    :: xiCL_NGeo(0:NGeo)   ,wBaryCL_NGeo(0:NGeo)
 REAL    :: xi0(3),dxi(3),length(3)
 
 #ifdef PARTICLES
@@ -183,6 +185,9 @@ CALL GetDerivativeMatrix(PP_N  , NodeTypeCL  , DCL_N)
 ! 2.d) derivatives (dXCL) by projection or by direct derivation (D_CL):
 CALL GetVandermonde(    PP_N   , NodeTypeCL  , PP_N    , NodeType,   Vdm_CLN_N         , modal=.FALSE.)
 CALL GetNodesAndWeights(PP_N   , NodeTypeCL  , xiCL_N  , wIPBary=wBaryCL_N)
+
+! 3.a) Interpolate from Tree for particls
+CALL GetNodesAndWeights(NGeo   , NodeTypeCL  , XiCL_NGeo  , wIPBary=wBaryCL_NGeo)
 
 ! Outer loop over all elements
 detJac_Ref=0.
@@ -377,14 +382,45 @@ DO iElem=1,nElems
                          NormVec,TangVec1,TangVec2,SurfElem,Face_xGP,Ja_Face)
   END IF
 
-  IF(PRESENT(XCL_Ngeo_Out))   XCL_Ngeo_Out(1:3,0:Ngeo,0:Ngeo,0:Ngeo,iElem)= XCL_Ngeo(1:3,0:Ngeo,0:Ngeo,0:Ngeo)
-  IF(PRESENT(dXCL_Ngeo_Out)) dXCL_Ngeo_Out(1:3,1:3,0:Ngeo,0:Ngeo,0:Ngeo,iElem)=dXCL_Ngeo(1:3,1:3,0:Ngeo,0:Ngeo,0:Ngeo)
+  ! particle mapping
+  IF(interpolateFromTree)THEN
+    IF((PRESENT(XCL_Ngeo_Out)).OR.(PRESENT(dXCL_NGeo_Out)))THEN
+      ! interpolate Metrics from Cheb-Lobatto N on tree level onto GaussPoints N on quad level
+      DO i=0,NGeo
+        dxi=0.5*(xiCL_NGeo(i)+1.)*length
+        CALL LagrangeInterpolationPolys(xi0(1) + dxi(1),NGeo,xiCL_NGeo,wBaryCL_NGeo,Vdm_xi_NGeo(  i,:))
+        CALL LagrangeInterpolationPolys(xi0(2) + dxi(2),NGeo,xiCL_NGeo,wBaryCL_NGeo,Vdm_eta_NGeo( i,:))
+        CALL LagrangeInterpolationPolys(xi0(3) + dxi(3),NGeo,xiCL_NGeo,wBaryCL_NGeo,Vdm_zeta_NGeo(i,:))
+      END DO
+      IF(PRESENT(XCL_Ngeo_Out))THEN
+        CALL ChangeBasis3D_XYZ(3,NGeo,NGeo,Vdm_xi_NGeo,Vdm_eta_NGeo,Vdm_zeta_NGeo, XCL_NGeo    (1:3,0:NGeo,0:NGeo,0:NGeo) &
+                                                                                 , XCL_NGeo_Out(1:3,0:NGeo,0:NGeo,0:NGeo,iElem))
+      END IF
+      IF(PRESENT(dXCL_nGeo_out))THEN
+        CALL ChangeBasis3D_XYZ(3,NGeo,NGeo,Vdm_xi_NGeo,Vdm_eta_NGeo,Vdm_zeta_NGeo,dXCL_NGeo    (1,1:3,0:NGeo,0:NGeo,0:NGeo) &
+                                                                                 ,dXCL_NGeo_Out(1,1:3,0:NGeo,0:NGeo,0:NGeo,iElem))
+        CALL ChangeBasis3D_XYZ(3,NGeo,NGeo,Vdm_xi_NGeo,Vdm_eta_NGeo,Vdm_zeta_NGeo,dXCL_NGeo    (2,1:3,0:NGeo,0:NGeo,0:NGeo) &
+                                                                                 ,dXCL_NGeo_Out(2,1:3,0:NGeo,0:NGeo,0:NGeo,iElem))
+        CALL ChangeBasis3D_XYZ(3,NGeo,NGeo,Vdm_xi_NGeo,Vdm_eta_NGeo,Vdm_zeta_NGeo,dXCL_NGeo    (3,1:3,0:NGeo,0:NGeo,0:NGeo) &
+                                                                                 ,dXCL_NGeo_Out(3,1:3,0:NGeo,0:NGeo,0:NGeo,iElem))
+      END IF
+    END IF
 #ifdef PARTICLES
-  CALL CPU_TIME(StartT2)
-  CALL GetBezierControlPoints3D(XCL_NGeo(:,:,:,:),iElem)
-  CALL CPU_TIME(endT)
-  BezierTime=BezierTime+endT-StartT2
+    CALL CPU_TIME(StartT2)
+    CALL GetBezierControlPoints3D(XCL_NGeo_Out(:,:,:,:,iElem),iElem)
+    CALL CPU_TIME(endT)
+    BezierTime=BezierTime+endT-StartT2
 #endif /*PARTICLES*/
+  ELSE
+    IF(PRESENT(XCL_Ngeo_Out))   XCL_Ngeo_Out(1:3,0:Ngeo,0:Ngeo,0:Ngeo,iElem)= XCL_Ngeo(1:3,0:Ngeo,0:Ngeo,0:Ngeo)
+    IF(PRESENT(dXCL_ngeo_out)) dXCL_Ngeo_Out(1:3,1:3,0:Ngeo,0:Ngeo,0:Ngeo,iElem)=dXCL_Ngeo(1:3,1:3,0:Ngeo,0:Ngeo,0:Ngeo)
+#ifdef PARTICLES
+    CALL CPU_TIME(StartT2)
+    CALL GetBezierControlPoints3D(XCL_NGeo(:,:,:,:),iElem)
+    CALL CPU_TIME(endT)
+    BezierTime=BezierTime+endT-StartT2
+#endif /*PARTICLES*/
+  END IF
 END DO !iElem=1,nElems
 
 #ifdef PARTICLES
