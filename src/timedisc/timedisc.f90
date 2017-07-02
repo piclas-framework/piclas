6a383c38812bed670a270b84bfd7ddd611670263
Stephen Copplestone <copplestone@iag.uni-stuttgart.de>
2017-07-02 22:35:43 +0200

    fixed memory corruption when dt=MINVAL((/dt_Min,tAnalyzeDiff,tEndDiff/)) is called with two uninitialized values

--- src/timedisc/timedisc.f90
+++ src/timedisc/timedisc.f90
@@ -341,6 +341,8 @@ iter_loc=0
 !IF(RP_onProc) CALL RecordPoints(iter,t,forceSampling=.TRUE.) 
 
 ! fill initial analyze stuff
+tAnalyzeDiff=tAnalyze-time    ! time to next analysis, put in extra variable so number does not change due to numerical errors
+tEndDiff=tEnd-time            ! dito for end time
 dt=MINVAL((/dt_Min,tAnalyzeDiff,tEndDiff/)) ! quick fix: set dt for initial write DSMCHOState (WriteMacroValues=T)
 CALL PerformAnalyze(time,iter,0.,forceAnalyze=.TRUE.,OutPut=.FALSE.)
 
