5ba5d7e841788c89172952132b2a68f98f5e5444
Philip Ortwein <ortwein@iag.uni-stuttgart.de>
2017-09-23 12:21:28 +0200

    missing force sampling.

--- src/particles/boundary/particle_boundary_condition.f90
+++ src/particles/boundary/particle_boundary_condition.f90
@@ -1270,6 +1270,24 @@ __STAMP__&
       nPartOut(PartSpecies(PartID))=nPartOut(PartSpecies(PartID)) + 1
       PartEkinOut(PartSpecies(PartID))=PartEkinOut(PartSpecies(PartID))+CalcEkinPart(PartID)
     END IF ! CalcPartBalance
+    ! sample values of deleted species
+    IF ((DSMC%CalcSurfaceVal.AND.(Time.ge.(1-DSMC%TimeFracSamp)*TEnd)).OR.(DSMC%CalcSurfaceVal.AND.WriteMacroSurfaceValues)) THEN
+      SurfSideID=SurfMesh%SideIDToSurfID(SideID)
+      Xitild =MIN(MAX(-1.,xi ),0.99)
+      Etatild=MIN(MAX(-1.,eta),0.99)
+      p=INT((Xitild +1.0)/dXiEQ_SurfSample)+1
+      q=INT((Etatild+1.0)/dXiEQ_SurfSample)+1
+      !----  Sampling Forces at walls
+  !       SampWall(SurfSideID)%State(10:12,p,q)= SampWall(SurfSideID)%State(10:12,p,q) + Species(PartSpecies(PartID))%MassIC &
+  !                                          * (v_old(1:3) - PartState(PartID,4:6)) * Species(PartSpecies(PartID))%MacroParticleFactor
+      SampWall(SurfSideID)%State(10,p,q)= SampWall(SurfSideID)%State(10,p,q) + Species(PartSpecies(PartID))%MassIC &
+                                            * ( PartState(PartID,4)) * Species(PartSpecies(PartID))%MacroParticleFactor
+      SampWall(SurfSideID)%State(11,p,q)= SampWall(SurfSideID)%State(11,p,q) + Species(PartSpecies(PartID))%MassIC &
+                                            * ( PartState(PartID,4)) * Species(PartSpecies(PartID))%MacroParticleFactor
+      SampWall(SurfSideID)%State(12,p,q)= SampWall(SurfSideID)%State(12,p,q) + Species(PartSpecies(PartID))%MassIC &
+                                            * ( PartState(PartID,4)) * Species(PartSpecies(PartID))%MacroParticleFactor
+    END IF
+    !---- Counter for collisions (normal wall collisions - not to count if only Swaps to be counted, IsSpeciesSwap: already counted)
     PDM%ParticleInside(PartID) = .FALSE.
     alpha=-1.
   ELSEIF (targetSpecies.gt.0) THEN !swap species
