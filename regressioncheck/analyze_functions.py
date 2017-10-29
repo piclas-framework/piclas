83825ee9cad0abdf457eacd69d72789049186273
Stephen Copplestone <copplestone@iag.uni-stuttgart.de>
2017-10-29 18:27:43 +0100

    reggie2.0: quick-fix divison by zero in order calculation, when error==0, set order=0

--- regressioncheck/analyze_functions.py
+++ regressioncheck/analyze_functions.py
@@ -38,8 +38,11 @@ def calcOrder_h(h,E) :
     order = []
     for i in range(1,len(h)) :
         dh=1.0/(h[i]/h[i-1])
-        dE=E[i]/E[i-1]
-        order.append(math.log(dE)/math.log(dh))
+        if E[i-1] == 0.0 :
+            order.append(0.0)
+        else :
+            dE=E[i]/E[i-1]
+            order.append(math.log(dE)/math.log(dh))
 
     return order
 
@@ -52,7 +55,10 @@ def calcOrder_p(p,E) :
     order = []
     for i in range(1,len(p)) :
         dp=1.0/((p[i]+1.0)/(p[i-1]+1.0))
-        dE=E[i]/E[i-1]
-        order.append(math.log(dE)/math.log(dp))
+        if E[i-1] == 0.0 :
+            order.append(0.0)
+        else :
+            dE=E[i]/E[i-1]
+            order.append(math.log(dE)/math.log(dp))
 
     return order
