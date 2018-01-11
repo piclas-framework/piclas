ca7ffeeafa890161fc40219fcc773353b035c901
Tilman Binder <binder@irs.uni-stuttgart.de>
2018-01-11 19:04:45 +0100

    timer for hdg similarly as for dg: save into tCurrent(1) and comm into tCurrent(2), but use later only tCurrent(1). Yes, this assumes that the load is prop. to number of elems (and by this always same number of sides), just for testing...
    (cherry picked from commit 33e7f9cc195ec834e17bdb16517dc63d5e8e42e1)

--- src/hdg/hdg.f90
+++ src/hdg/hdg.f90
@@ -597,6 +597,7 @@ USE MOD_Elem_Mat          ,ONLY:PostProcessGradient
 USE MOD_MPI_Vars
 USE MOD_MPI,           ONLY:FinishExchangeMPIData, StartReceiveMPIData,StartSendMPIData
 USE MOD_Mesh_Vars,     ONLY:nMPISides,nMPIsides_YOUR,nMPIsides_MINE
+USE MOD_LoadBalance_Vars,            ONLY:tCurrent
 #endif /*MPI*/ 
 #if (PP_nVar==1)
 USE MOD_Equation_Vars,     ONLY:E
@@ -627,7 +628,14 @@ INTEGER :: startbuf,endbuf
 #if (PP_nVar!=1)
 REAL    :: BTemp(3,3,nGP_vol,PP_nElems)
 #endif
+#ifdef MPI
+! load balance
+REAL    :: tLBStart,tLBEnd
+#endif /*MPI*/
 !===================================================================================================================================
+#ifdef MPI
+tLBStart = LOCALTIME() ! LB Time Start
+#endif /*MPI*/
 DO iVar = 1, PP_nVar
   !Dirichlet boundary conditions
 #if (PP_nVar!=1)
@@ -764,6 +772,10 @@ END DO
 
 
 #ifdef MPI
+tLBEnd = LOCALTIME() ! LB Time End
+tCurrent(1)=tCurrent(1)+tLBEnd-tLBStart
+
+  tLBStart = LOCALTIME() ! LB Time Start
   startbuf=nSides-nMPISides+1
   endbuf=nSides-nMPISides+nMPISides_MINE
   IF(nMPIsides_MINE.GT.0)RHS_face_buf=RHS_face(:,:,startbuf:endbuf)
@@ -772,9 +784,14 @@ END DO
   CALL FinishExchangeMPIData(SendRequest_U,     RecRequest_U,SendID=2) ! Send YOUR - receive MINE
   IF(nMPIsides_MINE.GT.0) RHS_face(:,:,startbuf:endbuf)=RHS_face(:,:,startbuf:endbuf)+RHS_face_buf
   IF(nMPIsides_YOUR.GT.0) RHS_face(:,:,nSides-nMPIsides_YOUR+1:nSides)=0. !set send buffer to zero!
+  tLBEnd = LOCALTIME() ! LB Time End
+  tCurrent(2)=tCurrent(2)+tLBEnd-tLBStart
 #endif /*MPI*/
 
     ! SOLVE 
+#ifdef MPI
+tLBStart = LOCALTIME() ! LB Time Start
+#endif /*MPI*/
 DO iVar=1, PP_nVar
   CALL CG_solver(RHS_face(iVar,:,:),lambda(iVar,:,:),iVar)
   !POST PROCESSING
@@ -820,6 +837,10 @@ END DO
   END DO; END DO; END DO !i,j,k
   CALL PostProcessGradient(U_out(4,:,:),lambda(4,:,:),E)
 #endif
+#ifdef MPI
+  tLBEnd = LOCALTIME() ! LB Time End
+  tCurrent(1)=tCurrent(1)+tLBEnd-tLBStart
+#endif /*MPI*/
 
 END SUBROUTINE HDGLinear
 
