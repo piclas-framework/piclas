943370309eff97cb44c59cc91c1a7b3cfa9e83ce
Stephen Copplestone <copplestone@iag.uni-stuttgart.de>
2018-04-28 17:56:05 +0200

    added new load balance routines to precond.f90

--- src/precond/precond.f90
+++ src/precond/precond.f90
@@ -149,22 +149,24 @@ SUBROUTINE BuildPrecond(t,tStage,tDeriv,alpha,dt)
 ! MODULES
 USE MOD_Globals
 USE MOD_PreProc
-USE MOD_Basis             ,ONLY:GetInverse
-USE MOD_Precond_Vars      ,ONLy:invXi,invEta,invZeta,dRdXi,dRdZeta,dRdEta
-USE MOD_LinearSolver_Vars ,ONLY:nDOFelem,mass,nDOFLine
-USE MOD_Precond_Vars      ,ONLY:invP,PrecondType,DebugMatrix
-USE MOD_Jac_ex            ,ONLY:Jac_ex, Jac_Ex_Neighbor,Jac_ex1D
-USE MOD_Jac_FD            ,ONLY:Jac_FD_slow!,Jac_FD
-USE MOD_JacDG             ,ONLY:BuildJacDG
-USE MOD_DG,                ONLY: DGTimeDerivative_WeakForm
-USE MOD_SparseILU,         ONLY: BuildILU0
-USE MOD_ILU,               ONLY: BuildBILU0BCSR
-USE MOD_CSR,               ONLY: CSR
-!USE MOD_CSR_Vars,      ONLY: DiagonalEntries
+USE MOD_Basis             ,ONLY: GetInverse
+USE MOD_Precond_Vars      ,ONLY: invXi,invEta,invZeta,dRdXi,dRdZeta,dRdEta
+USE MOD_LinearSolver_Vars ,ONLY: nDOFelem,mass,nDOFLine
+USE MOD_Precond_Vars      ,ONLY: invP,PrecondType,DebugMatrix
+USE MOD_Jac_ex            ,ONLY: Jac_ex, Jac_Ex_Neighbor,Jac_ex1D
+USE MOD_Jac_FD            ,ONLY: Jac_FD_slow                               
+USE MOD_JacDG             ,ONLY: BuildJacDG
+USE MOD_DG                ,ONLY: DGTimeDerivative_WeakForm
+USE MOD_SparseILU         ,ONLY: BuildILU0
+USE MOD_ILU               ,ONLY: BuildBILU0BCSR
+USE MOD_CSR               ,ONLY: CSR
 #ifdef MPI
 USE MOD_MPI_Vars
-USE MOD_LoadBalance_Vars,  ONLY: ElemTime
 #endif /*MPI*/
+#if USE_LOADBALANCE
+USE MOD_LoadBalance_Vars  ,ONLY: PerformLBSample
+USE MOD_LoadBalance_Vars  ,ONLY: ElemTime
+#endif /*USE_LOADBALANCE*/
 ! IMPLICIT VARIABLE HANDLING
 IMPLICIT NONE
 !-----------------------------------------------------------------------------------------------------------------------------------
@@ -348,9 +350,11 @@ DO iElem=1,PP_nElems
   END SELECT
   CALL CPU_TIME(TimeEnd(2))
   TotalTime=TotalTime+(TimeEnd-TimeStart)
-#ifdef MPI
-  ElemTime(iElem)=ElemTime(iElem)+SUM(TimeEnd(1:2)-TimeStart(1:2))
-#endif /*MPI*/
+#if USE_LOADBALANCE
+  IF(PerformLBSample)THEN
+    ElemTime(iElem)=ElemTime(iElem)+SUM(TimeEnd(1:2)-TimeStart(1:2))
+  END IF
+#endif /*USE_LOADBALANCE*/
   IF((PrecondType.LE.2).AND.DebugMatrix.NE.0)THEN
     CALL CheckBJPrecond(Ploc1,Ploc,invP(:,:,iElem),iElem,TotalTime(3))
   END IF ! DebugMatrix
