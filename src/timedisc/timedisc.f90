eacc90e18ab3d7d13e333d3723fa7c2808bf10c0
Stephen Copplestone <stephen@copplestone.de>
2018-11-20 13:47:52 +0100

    silly typo

--- src/timedisc/timedisc.f90
+++ src/timedisc/timedisc.f90
@@ -4575,7 +4575,7 @@ SUBROUTINE TimeStepPoissonByLSERK()
 ! MODULES
 USE MOD_Globals                ,ONLY: Abort, LocalTime
 USE MOD_PreProc
-USE MOD_TimeDisc_Vars          ,ONLY: dt,iStage,RKdtFrac,RKdtFracTotal,time,iterdt_Min,dtWeight
+USE MOD_TimeDisc_Vars          ,ONLY: dt,iStage,RKdtFrac,RKdtFracTotal,time,iter,dt_Min,dtWeight
 USE MOD_TimeDisc_Vars          ,ONLY: RK_a,RK_b,RK_c,nRKStages
 USE MOD_DG_Vars                ,ONLY: U
 #ifdef PARTICLES
