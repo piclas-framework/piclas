7fa7ae7b818823a92fd91d6319bf2743a3dad76a
Stephen Copplestone <stephen@copplestone.de>
2019-04-15 13:29:32 +0200

    changed cmake to ccmake in compile script

--- test_max_warnings.sh
+++ test_max_warnings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build_test_warnings
 cd build_test_warnings
-cmake tee -DCMAKE_BUILD_TYPE=DEBUG ..
+ccmake tee -DCMAKE_BUILD_TYPE=DEBUG ..
 make -j 2>&1 | tee compile_output.txt
 WARNINGS=$(grep "Warning:" compile_output.txt | wc -l)
 echo -e " "
