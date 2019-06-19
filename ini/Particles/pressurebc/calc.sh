2e342717c683c0176107b296b4da25a4811f712b
Wladimir Reschke <reschke@irs.uni-stuttgart.de>
2019-06-19 19:50:51 +0200

    removed uneccessary ini files

--- ini/Particles/pressurebc/calc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-STD_INDEX=$(ls std-*.out | tail -n 1 | sed s/std-/\\n/ | tail -n 1 | sed s/.out/\\n/ | head -n 1 )
-
-mpirun -np 32 piclas parameter_pressurebc.ini DSMCSpecies.ini 1>std-`printf "%03d" $((${STD_INDEX##*0}+1))`.out 2>err-`printf "%03d" $((${STD_INDEX##*0}+1))`.out
-wait
-vim std-`printf "%03d" $((${STD_INDEX##*0}+1))`.out
