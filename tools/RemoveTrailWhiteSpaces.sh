6aca90d77d24bb221be0198811151911be46dbc5
Wladimir Reschke <reschke@irs.uni-stuttgart.de>
2019-07-08 11:39:54 +0200

    added script for removing all trailing white spaces in all fortran files in gitroot/src directory

---
+++ tools/RemoveTrailWhiteSpaces.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# removes trailing white space in all fortran files found in [gitroot/src] directory
+SEARCHSTRING="\s\+$"
+REPLACESTRING=""
+FILESTRING="*.f90"
+SEARCHPATH="$(git rev-parse --show-toplevel)/src"
+FOUNDFILES=$(find "$SEARCHPATH" -iname "$FILESTRING")
+ACCEPTEDFILES=""
+for REPLACEFILE in ${FOUNDFILES}; do
+  if [[ -n $(grep -i "\s\+$" ${REPLACEFILE}) ]]; then
+    echo found $(grep -io "\s\+$" ${REPLACEFILE} | wc -l) trailing white space occurences in $REPLACEFILE
+    if [[ -z ${ACCEPTEDFILES} ]]; then
+      ACCEPTEDFILES+="${REPLACEFILE}"
+    else
+      ACCEPTEDFILES+="\n${REPLACEFILE}"
+    fi
+  fi
+done
+if [[ -n ${ACCEPTEDFILES} ]]; then
+  echo 'Do you want to remove trailing white spaces in these files?'
+  echo 'proceed: [y/n]'
+  read -es -n 1 PROCEED
+  if [[ "${PROCEED}" == 'y' ]]; then
+    for REPLACEFILE in ${FOUNDFILES}; do
+      if [[ -n $(grep -i "\s\+$" ${REPLACEFILE}) ]]; then
+        echo changes in file ${REPLACEFILE}
+        sed -i 's/'${SEARCHSTRING}'/'${REPLACESTRING}'/gI' ${REPLACEFILE}
+      fi
+    done
+  else
+    echo "no replaces done"
+  fi
+else
+  echo "no FORTRAN files with trailing white spaces found in gitroot/src"
+fi
