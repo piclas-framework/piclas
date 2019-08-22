fe14f6e973e38e666f658e4ba070030db80c45ff
Stephen Copplestone <Stephen@Copplestone.de>
2019-08-22 14:08:28 +0200

    fixed install script for MPI

--- tools/Setup_ModuleEnv/InstallMPIallCOMPILERS.sh
+++ tools/Setup_ModuleEnv/InstallMPIallCOMPILERS.sh
@@ -5,11 +5,11 @@ WHICHMPI=openmpi
 # choose for which compilers mpi is build (gcc or intel)
 WHICHCOMPILER=gcc
 
-if [ ${WHICHMPI} == openmpi ]; then
+if [ "${WHICHMPI}" == "openmpi" ]; then
   # DOWNLOAD and INSTALL OPENMPI (example OpenMPI-2.1.6)
   MPIVERSION=2.1.6
   MPIVERSIONTAG=2.1
-elif [ ${WHICHMPI} == mpich ]; then
+elif [ "${WHICHMPI}" == "mpich" ]; then
   # DOWNLOAD and INSTALL MPICH (example mpich-3.2.0)
   MPIVERSION=3.2
   MPIVERSIONTAG=3.2
@@ -19,7 +19,7 @@ else
   exit
 fi
 
-if [ ${WHICHCOMPILER} == gcc ] || [ ${WHICHCOMPILER} == intel ]; then
+if [ "${WHICHCOMPILER}" == "gcc" ] || [ "${WHICHCOMPILER}" == "intel" ]; then
   INSTALLDIR=/opt
   SOURCEDIR=/opt/Installsources
   MPIINSTALLDIR=${INSTALLDIR}/${WHICHMPI}/${MPIVERSION}
@@ -27,7 +27,7 @@ if [ ${WHICHCOMPILER} == gcc ] || [ ${WHICHCOMPILER} == intel ]; then
   MODULETEMPLATESDIR=/opt/Installsources/moduletemplates
   MODULETEMPLATENAME=template
 
-  if [ ! -d ${SOURCEDIR} ]; then
+  if [ ! -d "${SOURCEDIR}" ]; then
     mkdir -p ${SOURCEDIR}
   fi
 
@@ -45,7 +45,7 @@ if [ ${WHICHCOMPILER} == gcc ] || [ ${WHICHCOMPILER} == intel ]; then
       fi
     fi
     # if no mpi module for this compiler found, install ${WHICHMPI} and create module
-    if [ ! -e ${MPIMODULEFILE} ]; then
+    if [ ! -e "${MPIMODULEFILE}" ]; then
       echo "creating ${WHICHMPI}-${MPIVERSION} for ${WHICHCOMPILER}-${COMPILERVERSION}"
       module purge
       if [[ -n $(module load ${WHICHCOMPILER}/${COMPILERVERSION}) ]]; then
@@ -56,27 +56,27 @@ if [ ${WHICHCOMPILER} == gcc ] || [ ${WHICHCOMPILER} == intel ]; then
 
       # build and installation
       cd ${SOURCEDIR}
-      if [ ${WHICHMPI} == openmpi ]; then
-        if [ ! -e ${SOURCEDIR}/${WHICHMPI}-${MPIVERSION}.tar.gz ]; then
+      if [ "${WHICHMPI}" == "openmpi" ]; then
+        if [ ! -e "${SOURCEDIR}/${WHICHMPI}-${MPIVERSION}.tar.gz" ]; then
           wget "https://www.open-mpi.org/software/ompi/v${MPIVERSIONTAG}/downloads/openmpi-${MPIVERSION}.tar.gz"
         fi
-        if [ ! -e ${SOURCEDIR}/openmpi-${MPIVERSION}.tar.gz ]; then
+        if [ ! -e "${SOURCEDIR}/openmpi-${MPIVERSION}.tar.gz" ]; then
           echo "no mpi install-file downloaded for OpenMPI-${MPIVERSION}"
           echo "check if https://www.open-mpi.org/software/ompi/v${MPIVERSIONTAG}/downloads/openmpi-${MPIVERSION}.tar.gz exists"
           break
         fi
-      elif [ ${WHICHMPI} == mpich ]; then
-        if [ ! -e ${SOURCEDIR}/mpich-${MPIVERSION}.tar.gz ]; then
+      elif [ "${WHICHMPI}" == "mpich" ]; then
+        if [ ! -e "${SOURCEDIR}/mpich-${MPIVERSION}.tar.gz" ]; then
           wget "http://www.mpich.org/static/downloads/${MPIVERSION}/mpich-${MPIVERSION}.tar.gz"
         fi
-        if [ ! -e ${SOURCEDIR}/mpich-${MPIVERSION}.tar.gz ]; then
+        if [ ! -e "${SOURCEDIR}/mpich-${MPIVERSION}.tar.gz" ]; then
           echo "no mpi install-file downloaded for MPICH-${MPIVERSION}"
           echo "check if http://www.mpich.org/static/downloads/${MPIVERSION}/mpich-${MPIVERSION}.tar.gz exists"
           break
         fi
       fi
       tar -xzf ${WHICHMPI}-${MPIVERSION}.tar.gz
-      if [ ! -e ${SOURCEDIR}/${WHICHMPI}-${MPIVERSION}/build_${WHICHCOMPILER}-${COMPILERVERSION} ]; then
+      if [ ! -e "${SOURCEDIR}/${WHICHMPI}-${MPIVERSION}/build_${WHICHCOMPILER}-${COMPILERVERSION}" ]; then
         mkdir -p ${SOURCEDIR}/${WHICHMPI}-${MPIVERSION}/build_${WHICHCOMPILER}-${COMPILERVERSION}
       fi
       if [[ ${1} =~ ^-r(erun)?$ ]] ; then
@@ -84,17 +84,17 @@ if [ ${WHICHCOMPILER} == gcc ] || [ ${WHICHCOMPILER} == intel ]; then
       fi
       cd ${SOURCEDIR}/${WHICHMPI}-${MPIVERSION}/build_${WHICHCOMPILER}-${COMPILERVERSION}
 
-      if [ ${WHICHCOMPILER} == gcc ]; then
+      if [ "${WHICHCOMPILER}" == "gcc" ]; then
         ../configure --prefix=${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION} CC=$(which gcc) CXX=$(which g++) FC=$(which gfortran)
-      elif [ ${WHICHCOMPILER} == intel ]; then
+      elif [ "${WHICHCOMPILER}" == "intel" ]; then
         ../configure --prefix=${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION} CC=$(which icc) CXX=$(which icpc) FC=$(which ifort)
       fi
       make -j 2 2>&1 | tee make.out
       make install 2>&1 | tee install.out
 
       # create modulefile if installation seems succesfull (check if mpicc, mpicxx, mpifort exists in installdir)
-      if [ -e ${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION}/bin/mpicc ] && [ -e ${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION}/bin/mpicxx ] && [ -e ${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION}/bin/mpifort ]; then
-        if [ ! -d ${MPIMODULEFILEDIR} ]; then
+      if [ -e "${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION}/bin/mpicc" ] && [ -e "${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION}/bin/mpicxx" ] && [ -e "${MPIINSTALLDIR}/${WHICHCOMPILER}/${COMPILERVERSION}/bin/mpifort" ]; then
+        if [ ! -d "${MPIMODULEFILEDIR}" ]; then
           mkdir -p ${MPIMODULEFILEDIR}
         fi
         cp ${MODULETEMPLATESDIR}/MPI/${MODULETEMPLATENAME} ${MPIMODULEFILE}
