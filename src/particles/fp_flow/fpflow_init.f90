cfd9436380f2a7fe1c7abdfd5f35f1b7b87226a6
Marcel Pfeiffer <mpfeiffer@irs.uni-stuttgart.de>
2020-01-29 13:43:11 +0000

    Update fpflow_init.f90

--- src/particles/fp_flow/fpflow_init.f90
+++ src/particles/fp_flow/fpflow_init.f90
@@ -79,7 +79,7 @@ USE MOD_Preproc               ,ONLY: PP_N
 USE MOD_Mesh_Vars             ,ONLY: NGeo, nElems
 USE MOD_Globals_Vars          ,ONLY: PI, BoltzmannConst
 USE MOD_ReadInTools
-USE MOD_DSMC_Vars             ,ONLY: SpecDSMC, DSMC, RadialWeighting
+USE MOD_DSMC_Vars             ,ONLY: SpecDSMC, DSMC
 USE MOD_DSMC_ParticlePairing  ,ONLY: DSMC_init_octree
 USE MOD_PARTICLE_Vars         ,ONLY: nSpecies, Species
 USE MOD_FPFlow_Vars
