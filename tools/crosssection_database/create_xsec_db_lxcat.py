8f318f19e57bd2e41063337a0fea7fde3ac4c342
Paul Nizenkov <p.nizenkov@gmail.com>
2020-03-07 13:28:36 +0100

    Documentation cross-section database

--- tools/crosssection_database/create_xsec_db_lxcat.py
+++ tools/crosssection_database/create_xsec_db_lxcat.py
@@ -2,15 +2,24 @@ import numpy as np
 import h5py
 import lxcat_data_parser as ldp
 
-hdf = h5py.File('LXCat_Database_Phelps_Electron_Scattering_EFFECTIVE.h5', 'w')
-hdf.attrs['Info'] = 'Effective electron-neutrals collision cross-section database for read-in with PICLas. First column is the collision energy in [eV], second column is the cross-section in [m^2]'
-hdf.attrs['Source'] = 'Phelps database, www.lxcat.net, retrieved on February 18, 2020.'
-
+# Input database
+database_input = "Database_Phelps.txt"
+# Output database
+database_output = "LXCat_Database_Phelps_Electron_Scattering_EFFECTIVE.h5"
+# Species list to be included in the output database
 species_list = ["Ar","CO","CO2","H2","H2O","He","Mg","N2","NO","Na","Ne","O2","SF6"]
+# Reference of the utilized database
+reference = "Phelps database, www.lxcat.net, retrieved on February 18, 2020. LXCat is an open-access website with databases contributed by members of the scientific community."
 
+# Output of the HDF5 database
+hdf = h5py.File(database_output, 'w')
+hdf.attrs['Info'] = 'Effective electron-neutrals collision cross-section database for read-in with PICLas. First column is the collision energy in [eV], second column is the cross-section in [m^2]'
+hdf.attrs['Reference'] = reference
+
+# Read-in of data and output of the species containers
 for current_species in species_list:
     print(current_species)
-    data_spec = ldp.CrossSectionSet("Database_Phelps.txt",imposed_species=current_species)
+    data_spec = ldp.CrossSectionSet(database_input,imposed_species=current_species)
     for cross_section in data_spec.cross_sections:
         # For now, only considering effective cross-sections in PICLas
         if cross_section.type == ldp.CrossSectionTypes.EFFECTIVE:
