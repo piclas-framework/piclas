e64026a8f0c22274b4818b962330428746fe337c
Stephen Copplestone <stephen@copplestone.de>
2020-07-09 08:17:23 +0200

    Added global number of particles to ElemTimeStatistics.csv

--- src/loadbalance/loaddistribution.f90
+++ src/loadbalance/loaddistribution.f90
@@ -1103,11 +1103,9 @@ SUBROUTINE WriteElemTimeStatistics(WriteHeader,time,iter)
 ! MODULES                                                                                                                          !
 !----------------------------------------------------------------------------------------------------------------------------------!
 USE MOD_LoadBalance_Vars ,ONLY: TargetWeight,nLoadBalanceSteps,CurrentImbalance,MinWeight,MaxWeight,WeightSum
-USE MOD_Globals          ,ONLY: MPIRoot,FILEEXISTS,unit_stdout
+USE MOD_Globals          ,ONLY: MPIRoot,FILEEXISTS,unit_stdout,abort,nProcessors,nGlobalNbrOfParticles
 USE MOD_Globals_Vars     ,ONLY: SimulationEfficiency,PID,WallTime,InitializationWallTime
 USE MOD_Restart_Vars     ,ONLY: DoRestart
-USE MOD_Globals          ,ONLY: abort
-USE MOD_Globals          ,ONLY: nProcessors
 USE MOD_LoadBalance_Vars ,ONLY: ElemTimeField
 #ifdef PARTICLES
 USE MOD_LoadBalance_Vars ,ONLY: ElemTimePart
@@ -1126,9 +1124,9 @@ INTEGER                                  :: ioUnit,I
 CHARACTER(LEN=150)                       :: formatStr
 #ifdef PARTICLES
 REAL                                     :: SumElemTime,ElemTimeFieldPercent,ElemTimePartPercent
-INTEGER,PARAMETER                        :: nOutputVar=16
+INTEGER,PARAMETER                        :: nOutputVar=17
 #else
-INTEGER,PARAMETER                        :: nOutputVar=12
+INTEGER,PARAMETER                        :: nOutputVar=13
 #endif /*PARTICLES*/
 CHARACTER(LEN=255),DIMENSION(nOutputVar) :: StrVarNames(nOutputVar)=(/ CHARACTER(LEN=255) :: &
     'time'                   , &
@@ -1144,7 +1142,8 @@ CHARACTER(LEN=255),DIMENSION(nOutputVar) :: StrVarNames(nOutputVar)=(/ CHARACTER
     'SimulationWallTime'     , &
     'InitializationWallTime'   &
 #ifdef PARTICLES
-  , 'FieldTime'              , &
+  , '#Particles'             , &
+    'FieldTime'              , &
     'PartTime'               , &
     'FieldTimePercent'       , &
     'PartTimePercent'          &
@@ -1207,20 +1206,21 @@ ELSE !
     OPEN(NEWUNIT=ioUnit,FILE=TRIM(outfile),POSITION="APPEND",STATUS="OLD")
     WRITE(formatStr,'(A2,I2,A14,A1)')'(',nOutputVar,CSVFORMAT,')'
     WRITE(tmpStr2,formatStr)&
-              " ",time_loc, &
-        delimiter,REAL(nProcessors), &
-        delimiter,MinWeight, &
-        delimiter,MaxWeight, &
-        delimiter,CurrentImbalance, &
-        delimiter,TargetWeight, &
-        delimiter,REAL(nLoadBalanceSteps), &
-        delimiter,WeightSum, &
+              " ",time_loc,&
+        delimiter,REAL(nProcessors),&
+        delimiter,MinWeight,&
+        delimiter,MaxWeight,&
+        delimiter,CurrentImbalance,&
+        delimiter,TargetWeight,&
+        delimiter,REAL(nLoadBalanceSteps),&
+        delimiter,WeightSum,&
         delimiter,SimulationEfficiency,&
         delimiter,PID,&
         delimiter,WallTime,&
         delimiter,InitializationWallTime&
 #ifdef PARTICLES
-       ,delimiter,ElemTimeField,&
+       ,delimiter,REAL(nGlobalNbrOfParticles),&
+        delimiter,ElemTimeField,&
         delimiter,ElemTimePart,&
         delimiter,ElemTimeFieldPercent,&
         delimiter,ElemTimePartPercent
