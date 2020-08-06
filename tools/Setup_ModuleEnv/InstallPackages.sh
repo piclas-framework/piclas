82ec80e590f3deae9dd7e1cc6d3717da9399b35a
Stephen <copplestone@iag.uni-stuttgart.de>
2020-08-06 13:41:32 +0200

    Cleanup of original install script including all used libs for install

--- tools/Setup_ModuleEnv/InstallPackages.sh
+++ tools/Setup_ModuleEnv/InstallPackages.sh
@@ -11,35 +11,64 @@
 # notes       : 
 #==============================================================================
 
-apt-get update
-apt-get install \
-  libgmp3-dev \
-  libmpfr-dev \
-  libmpfr-doc \
-  libmpfr6 \
-  libmpc-dev \
-  build-essential \
-  libc6-dev \
-  binutils \
-  libblas-dev \
-  liblapack-dev \
-  libpython-stdlib \
-  libpython-dev \
-  make \
-  libphonon-dev \
-  qt4-qmake \
-  qt4-dev-tools \
-  libqt4-opengl-dev \
-  libqt4-dev \
-  libxt-dev \
-  mesa-common-dev \
-  libz-dev \
-  libncurses-dev \
-  tcl \
-  tcl8.6-dev \
-  ctags \
-  libboost-dev \
-  libgl1-mesa-dri
+# Check for updates
+sudo apt-get update
 
-mkdir -p /opt/Installsources
-cp -r moduletemplates /opt/Installsources/moduletemplates
+# filesystem operations
+sudo apt-get install  libboost-dev
+
+# cmake
+sudo apt-get install  libncurses-dev
+
+# ctags 
+sudo apt-get install  ctags
+
+# paraview
+sudo apt-get install  qt4-qmake  qt4-dev-tools  libqt4-opengl-dev  libqt4-dev 
+
+# libphonon-dev (multimedia framework from KDE using Qt 4 - development files)
+sudo apt-get install  libphonon-dev
+
+# make
+sudo apt-get install  make
+
+# python
+sudo apt-get install  libpython-stdlib  libpython-dev
+
+# blas and lapack
+sudo apt-get install  libblas-dev  liblapack-dev
+
+# tcl (required for module install scripts)
+sudo apt-get install  tcl  tcl8.6-dev
+
+# mesa (Mesa-specific OpenGL extensions)
+sudo apt-get install  mesa-common-dev  libgl1-mesa-dri
+
+# libz (compression library - development)
+sudo apt-get install  libz-dev
+
+# libxt (toolkit intrinsics library - development headers)
+sudo apt-get install  libxt-dev
+
+# Binutils are a collection of binary tools
+sudo apt-get install  binutils
+
+# GNU C Library: Development Libraries and Header Files
+sudo apt-get install  libc6-dev
+
+# Reference for all the packages needed to compile a Debian package.
+# It generally includes the GCC/g++ compilers and libraries and some other utilities.
+sudo apt-get install  build-essential
+
+# multiple precision complex floating-point library development package
+sudo apt-get install  libmpc-dev
+
+# Multiprecision arithmetic library developers tools
+sudo apt-get install  libgmp3-dev
+
+# multiple precision floating-point computation developers tools
+sudo apt-get install  libmpfr-dev  libmpfr-doc  libmpfr6
+
+# Create Installsources directory and copy the module templates
+sudo mkdir -p /opt/Installsources
+sudo cp -r moduletemplates /opt/Installsources/moduletemplates
