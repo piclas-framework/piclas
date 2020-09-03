efb58466163f4100407a81647374f7a5bdcaba52
Paul Nizenkov <p.nizenkov@gmail.com>
2020-09-03 17:13:15 +0200

    Replaced loop for zeroing the second/third variable in MacroBody

--- src/timedisc/timedisc.f90
+++ src/timedisc/timedisc.f90
@@ -1678,15 +1678,11 @@ REAL                       :: tLBStart
   CALL LBSplitTime(LB_PUSH,tLBStart)
 #endif /*USE_LOADBALANCE*/
 
-  ! Resetting the particle positions in the third dimension for the 2D/axisymmetric case
-  ! IF(Symmetry%Order.EQ.2) THEN
-  !   LastPartPos(3,1:PDM%ParticleVecLength) = 0.0
-  !   PartState(3,1:PDM%ParticleVecLength) = 0.0
-  ! END IF
-  DO iLoop=Symmetry%Order+1,3
-    LastPartPos(iLoop,1:PDM%ParticleVecLength) = 0.0
-    PartState(iLoop,1:PDM%ParticleVecLength) = 0.0
-  END DO
+  ! Resetting the particle positions in the second/third dimension for the 1D/2D/axisymmetric case
+  IF(Symmetry%Order.LT.3) THEN
+    LastPartPos(Symmetry%Order+1:3,1:PDM%ParticleVecLength) = 0.0
+    PartState(Symmetry%Order+1:3,1:PDM%ParticleVecLength) = 0.0
+  END IF
 
 #if USE_MPI
   ! open receive buffer for number of particles
