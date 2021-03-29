fee0210359b1f157a1ea5c4c28aae3b18f0dc8b9
Paul Nizenkov <p.nizenkov@gmail.com>
2021-03-30 00:05:55 +0200

    Script for reactions from the NIFS database: user input as single reaction per file

--- tools/crosssection_database/write_reaction_xsec_nifs.py
+++ tools/crosssection_database/write_reaction_xsec_nifs.py
@@ -1,65 +1,53 @@
 import numpy as np
 import h5py
 
+### User-input
 # Output database
 database_output = "XSec_Database_Xe_Plasma.h5"
-# Open existing file/create new
-hdf = h5py.File(database_output, 'r+')
-hdf.attrs['Date'] = '16.03.2021'
-
-## User-input
-# pair = 'Xe-electron'
-# reaction = 'XeIon1-electron-electron'
-# # Read-in input file, skip the first 8 lines of the header (NIFS output copied starting with Record No)
-# data_input = np.genfromtxt('NIFS-Xe-elec-XeIon-2elec.dat', dtype=float, delimiter=',', skip_header=8, usecols=(0,1))
-# source_input = 'NIFS Database: Krishnakumar, E et al. J. Phys. B 21 (1988) 1055'
-########################################################################################################
-## User-input
-# pair = 'Xe-electron'
-# reaction = 'XeIon2-electron-electron-electron'
-# # Read-in input file, skip the first 8 lines of the header (NIFS output copied starting with Record No)
-# data_input = np.genfromtxt('NIFS-Xe-elec-XeIon2-3elec.dat', delimiter=',', skip_header=8, usecols=(0,1))
-# source_input = 'NIFS Database: Krishnakumar, E et al. J. Phys. B 21 (1988) 1055'
-########################################################################################################
-## User-input
-pair = 'XeIon1-electron'
-reaction = 'XeIon2-electron-electron'
+# Reactants and products
+pair_list =     ['Xe-electron',                 'Xe-electron',                      'XeIon1-electron']
+reaction_list = ['XeIon1-electron-electron',    'XeIon2-electron-electron-electron','XeIon2-electron-electron']
 # Read-in input file, skip the first 8 lines of the header (NIFS output copied starting with Record No)
-data_input = np.genfromtxt('NIFS-XeIon-elec-XeIon2-2e.dat', delimiter=',', skip_header=8, usecols=(0,1))
-source_input = 'NIFS Database: Man, K.F. et al. J. Phys. B 20 (1987) 5865'
+input_list =    ['NIFS-Xe-elec-XeIon-2elec.dat','NIFS-Xe-elec-XeIon2-3elec.dat',    'NIFS-XeIon-elec-XeIon2-2e.dat']
+source_list =   ['NIFS Database: Krishnakumar, E et al. J. Phys. B 21 (1988) 1055','NIFS Database: Krishnakumar, E et al. J. Phys. B 21 (1988) 1055','NIFS Database: Man, K.F. et al. J. Phys. B 20 (1987) 5865']
 ########################################################################################################
 
-# Sort data input by ascending energy values
-print(data_input.view('f8,f8').sort(order=['f0'], axis=0))
+hdf = h5py.File(database_output, 'r+')
+for iReac, current_reaction in enumerate(reaction_list):
+  print('Reactants: ' + pair_list[iReac])
+  print('Products: ' + current_reaction)
+  data_input = np.genfromtxt(input_list[iReac], delimiter=',', skip_header=8, usecols=(0,1))
+  # Sort data input by ascending energy values
+  data_input.view('f8,f8').sort(order=['f0'], axis=0)
 
-# Convert cm2 to m2
-data_input[:,1] *= 1E-4
-print('Input converted from cm2 to m2.')
+  # Convert cm2 to m2
+  data_input[:,1] *= 1E-4
+  print('Input converted from cm2 to m2.')
 
-# Check if the collision pair (=folder) already exists, if not create it
-hdf_pair = pair
-if hdf_pair in hdf.keys():
-  print('Collision pair already exists.')
-  hdf_pair = hdf[pair]
-else:
-  print('Collision pair does not exist, creating new pair.')
-  hdf_pair = hdf.create_group(pair)
+  # Check if the collision pair (=folder) already exists, if not create it
+  hdf_pair = pair_list[iReac]
+  if hdf_pair in hdf.keys():
+    print('Collision pair already exists.')
+    hdf_pair = hdf[pair_list[iReac]]
+  else:
+    print('Collision pair does not exist, creating new pair.')
+    hdf_pair = hdf.create_group(pair_list[iReac])
 
-# Check if a REACTION group (=folder) already exists, if not create it
-hdf_reaction = 'REACTION'
-if hdf_reaction in hdf_pair.keys():
-  print('Group REACTION already exists.')
-  hdf_reaction = hdf_pair['REACTION']
-else:
-  print('Group REACTION does not exist, creating new group.')
-  hdf_reaction = hdf_pair.create_group('REACTION')
+  # Check if a REACTION group (=folder) already exists, if not create it
+  hdf_reaction = 'REACTION'
+  if hdf_reaction in hdf_pair.keys():
+    print('Group REACTION already exists.')
+    hdf_reaction = hdf_pair['REACTION']
+  else:
+    print('Group REACTION does not exist, creating new group.')
+    hdf_reaction = hdf_pair.create_group('REACTION')
 
-# If reaction data set already exists, delete the old set
-if reaction in hdf_reaction.keys():
-  del hdf_reaction[reaction]
-  print('Old dataset replaced.')
-hdf_reaction.create_dataset(reaction, data=data_input)
+  # If reaction data set already exists, delete the old set
+  if current_reaction in hdf_reaction.keys():
+    del hdf_reaction[current_reaction]
+    print('Old dataset replaced.')
+  hdf_reaction.create_dataset(current_reaction, data=data_input)
 
-hdf_reaction[reaction].attrs['Source'] = source_input
+  hdf_reaction[current_reaction].attrs['Source'] = source_list[iReac]
 
 hdf.close()
