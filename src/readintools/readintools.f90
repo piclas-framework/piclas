3f3a3cfd34381d52a8a1753a9b42563b83631b86
Stephen Copplestone <stephen@copplestone.de>
2021-07-14 20:15:57 +0200

    Added missing allocate ALLOCATE(CHARACTER(LEN_TRIM(name)) :: testname) for CHARACTER(LEN=:),ALLOCATABLE :: testname

--- src/readintools/readintools.f90
+++ src/readintools/readintools.f90
@@ -1258,6 +1258,7 @@ DO WHILE (associated(current))
       newopt%numberedmulti = .FALSE.
       newopt%isSet = .FALSE.
       ! Check if we can find a general option, applying to all numberedmulti
+      ALLOCATE(CHARACTER(LEN_TRIM(name)) :: testname)
       testname = name
       DO i = 1, LEN(name)
         ! Start replacing the index from the left
@@ -1457,6 +1458,7 @@ DO WHILE (associated(current))
       newopt%numberedmulti = .FALSE.
       newopt%isSet = .FALSE.
       ! Check if we can find a general option, applying to all numberedmulti
+      ALLOCATE(CHARACTER(LEN_TRIM(name)) :: testname)
       testname = name
       DO i = 1, LEN(name)
         ! Start replacing the index from the left
