2182fe1ea831de407344b347affac06cd165b6bb
Stephen Copplestone <stephen@copplestone.de>
2021-10-19 17:42:57 +0200

    Updated install scripts for ubuntu 21 systems

---
+++ tools/Setup_ModuleEnv/InstallPackagesUbuntu21.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#==============================================================================
+# title       : InstallPackages.sh
+# description : This script installs the software packages required for
+#               the module env scripts for creating a software environment for
+#               PICLas/FLEXI code frameworks
+# date        : Nov 27, 2019
+# version     : 1.0
+# usage       : bash InstallPackages.sh
+# notes       :
+#==============================================================================
+
+# Check for updates
+sudo apt-get update
+
+# compiler
+sudo apt-get install make cmake cmake-curses-gui gfortran g++ gcc -y
+
+# filesystem operations
+sudo apt-get install  libboost-dev -y
+
+# cmake: developer's libraries for ncurses
+sudo apt-get install  libncurses-dev  libncurses5-dev -y
+
+# ctags
+sudo apt-get install  exuberant-ctags -y
+
+# blas and lapack
+sudo apt-get install  libblas-dev  liblapack3  liblapack-dev -y
+
+# tcl (required for module install scripts)
+sudo apt-get install  tcl  tcl8.6-dev -y
+
+# zlib is a library implementing the deflate compression method found in gzip and PKZIP (development)
+sudo apt-get install   zlib1g-dev -y
+
+# Secure Sockets Layer toolkit - development files
+sudo apt-get install  libssl-dev -y
--- tools/Setup_ModuleEnv/README.txt
+++ tools/Setup_ModuleEnv/README.txt
@@ -4,14 +4,21 @@ These scripts help setting up an environment for development with piclas
 Order of how to setup environment
 
 0. change dir to script directory ( cd /path/to/piclas/tools )
+
 1. Install the basic packages depending on the OS
    - ./InstallPackagesUbuntu16.sh
    - ./InstallPackagesUbuntu20.sh
+   - ./InstallPackagesUbuntu21.sh
+   and if you have a server that has been setup with only basic packages, the following might be required
+   sudo ./InstallPackagesServer.sh
+
 2. open a new terminal and run:
       sudo -s
    or run the following script with sudo ./Install...
+
 3. ./InstallModules.sh
   4. reboot and maybe second time ./InstallModules.sh is needed
+
 5. ./InstallCMake.sh
 6. ./InstallGCC.sh
 7. ./InstallMPIallCOMPILERS.sh
