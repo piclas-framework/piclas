2cde651855d5ed637e066bfc880e100779c798be
Stephen Copplestone <stephen@copplestone.de>
2021-11-30 21:41:59 +0100

    Fix if clause in generate userblock script

--- tools/userblock/generateuserblock.sh
+++ tools/userblock/generateuserblock.sh
@@ -84,7 +84,7 @@ echo $PARENTCOMMIT                 >> userblock.txt
 # Also store binary changes in diff
 echo "{[( GIT DIFF )]}"            >> userblock.txt
 # commited changes
-if [[ -n ${PARENTEXISTS} ]]; then
+if [ -n ${PARENTEXISTS} ]; then
   git diff -p $PARENTCOMMIT..HEAD | head -n 1000   >> userblock.txt
 fi
 # uncommited changes
