e4fb056d90d6cc102f40f8a53626bfda1f545d07
Paul Nizenkov <p.nizenkov@gmail.com>
2023-01-07 15:37:24 +0100

    Do not consider the species-specific time step for the surface flux during a restart

--- src/particles/emission/particle_surface_flux_init.f90
+++ src/particles/emission/particle_surface_flux_init.f90
@@ -302,13 +302,13 @@ IF(DoRestart) THEN
       ELSE
         VFR_total = Species(iSpec)%Surfaceflux(iSF)%VFR_total               !proc local total
       END IF
-      ! Species-specific time step
-      IF(VarTimeStep%UseSpeciesSpecific) THEN
-        RestartTimeVar = RestartTime * Species(iSpec)%TimeStepFactor
-      ELSE
-        RestartTimeVar = RestartTime
-      END IF
-      Species(iSpec)%Surfaceflux(iSF)%InsertedParticle = INT(Species(iSpec)%Surfaceflux(iSF)%PartDensity * RestartTimeVar &
+      ! ! Species-specific time step
+      ! IF(VarTimeStep%UseSpeciesSpecific) THEN
+      !   RestartTimeVar = RestartTime * Species(iSpec)%TimeStepFactor
+      ! ELSE
+      !   RestartTimeVar = RestartTime
+      ! END IF
+      Species(iSpec)%Surfaceflux(iSF)%InsertedParticle = INT(Species(iSpec)%Surfaceflux(iSF)%PartDensity * RestartTime &
         / Species(iSpec)%MacroParticleFactor * VFR_total,8)
     END DO
   END DO
