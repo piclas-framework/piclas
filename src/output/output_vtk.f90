7d4d2fdf5261ec4feb31bcb081560a2147ac00f9
Patrick Kopper <patrick.kopper@ila.uni-stuttgart.de>
2023-06-12 15:29:17 +0200

    Fixes for GCC13

--- src/output/output_vtk.f90
+++ src/output/output_vtk.f90
@@ -180,8 +180,12 @@ LOGICAL                     :: nValAtLastDimension_loc
 REAL                        :: StartT,EndT ! Timer
 !===================================================================================================================================
 GETTIME(StartT)
-DGFV_loc = MERGE(DGFV, 0, PRESENT(DGFV))
-nValAtLastDimension_loc = MERGE(nValAtLastDimension, .FALSE., PRESENT(nValAtLastDimension))
+IF (PRESENT(DGFV))                THEN; DGFV_loc = DGFV
+ELSE;                                   DGFV_loc = 0
+END IF
+IF (PRESENT(nValAtLastDimension)) THEN; nValAtLastDimension_loc = nValAtLastDimension
+ELSE;                                   nValAtLastDimension_loc = .FALSE.
+END IF
 IF (dim.EQ.3) THEN
   NVisu_k = NVisu
   NVisu_j = NVisu
