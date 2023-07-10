flexi-framework/elexi

Mapping Global<->CN is only allocated with nComputeNodeProcessors.NE.nProcessors_Global

src/particles/mesh/particle_bgm.f90
@@ -1487,13 +1487,15 @@ IF (.NOT.PerformLoadBalance) THEN
   CALL MPI_WIN_FREE(BoundsOfElem_Shared_Win,iError)
   CALL MPI_WIN_UNLOCK_ALL(FIBGM_nTotalElems_Shared_Win,iError)
   CALL MPI_WIN_FREE(FIBGM_nTotalElems_Shared_Win,iError)
-  CALL MPI_WIN_UNLOCK_ALL(GlobalElem2CNTotalElem_Shared_Win,iError)
-  CALL MPI_WIN_FREE(GlobalElem2CNTotalElem_Shared_Win,iError)
-  CALL MPI_WIN_UNLOCK_ALL(GlobalSide2CNTotalSide_Shared_Win,iError)
-  CALL MPI_WIN_FREE(GlobalSide2CNTotalSide_Shared_Win,iError)
   ! ElemToBGM is only used during init. But it is expensive to build, so keep it around for now
   CALL MPI_WIN_UNLOCK_ALL(ElemToBGM_Shared_Win,iError)
   CALL MPI_WIN_FREE(ElemToBGM_Shared_Win,iError)
+  IF (nComputeNodeProcessors.NE.nProcessors_Global) THEN
+    CALL MPI_WIN_UNLOCK_ALL(GlobalElem2CNTotalElem_Shared_Win,iError)
+    CALL MPI_WIN_FREE(GlobalElem2CNTotalElem_Shared_Win,iError)
+    CALL MPI_WIN_UNLOCK_ALL(GlobalSide2CNTotalSide_Shared_Win,iError)
+    CALL MPI_WIN_FREE(GlobalSide2CNTotalSide_Shared_Win,iError)
+  END IF ! nComputeNodeProcessors.NE.nProcessors_Global
 #if USE_LOADBALANCE
 END IF
 #endif /*USE_LOADBALANCE*/
@@ -1507,10 +1509,12 @@ CALL MPI_WIN_UNLOCK_ALL(FIBGMToProc_Shared_Win,iError)
 CALL MPI_WIN_FREE(FIBGMToProc_Shared_Win,iError)
 CALL MPI_WIN_UNLOCK_ALL(FIBGMProcs_Shared_Win,iError)
 CALL MPI_WIN_FREE(FIBGMProcs_Shared_Win,iError)
-CALL MPI_WIN_UNLOCK_ALL(CNTotalElem2GlobalElem_Shared_Win,iError)
-CALL MPI_WIN_FREE(CNTotalElem2GlobalElem_Shared_Win,iError)
-CALL MPI_WIN_UNLOCK_ALL(CNTotalSide2GlobalSide_Shared_Win,iError)
-CALL MPI_WIN_FREE(CNTotalSide2GlobalSide_Shared_Win,iError)
+IF (nComputeNodeProcessors.NE.nProcessors_Global) THEN
+  CALL MPI_WIN_UNLOCK_ALL(CNTotalElem2GlobalElem_Shared_Win,iError)
+  CALL MPI_WIN_FREE(CNTotalElem2GlobalElem_Shared_Win,iError)
+  CALL MPI_WIN_UNLOCK_ALL(CNTotalSide2GlobalSide_Shared_Win,iError)
+  CALL MPI_WIN_FREE(CNTotalSide2GlobalSide_Shared_Win,iError)
+END IF ! nComputeNodeProcessors.NE.nProcessors_Global
 
 CALL MPI_BARRIER(MPI_COMM_SHARED,iERROR)
 
@@ -1520,11 +1524,13 @@ IF (.NOT.PerformLoadBalance) THEN
 #endif /*USE_LOADBALANCE*/
   MDEALLOCATE(FIBGM_nTotalElems)
   MDEALLOCATE(FIBGM_nTotalElems_Shared)
-  MDEALLOCATE(GlobalElem2CNTotalElem)
-  MDEALLOCATE(GlobalElem2CNTotalElem_Shared)
-  MDEALLOCATE(GlobalSide2CNTotalSide)
-  MDEALLOCATE(GlobalSide2CNTotalSide_Shared)
   MDEALLOCATE(ElemToBGM_Shared)
+  IF (nComputeNodeProcessors.NE.nProcessors_Global) THEN
+    MDEALLOCATE(GlobalElem2CNTotalElem)
+    MDEALLOCATE(GlobalElem2CNTotalElem_Shared)
+    MDEALLOCATE(GlobalSide2CNTotalSide)
+    MDEALLOCATE(GlobalSide2CNTotalSide_Shared)
+  END IF ! nComputeNodeProcessors.NE.nProcessors_Global
 #if USE_LOADBALANCE
 END IF
 #endif /*USE_LOADBALANCE*/
@@ -1547,10 +1553,12 @@ MDEALLOCATE(FIBGMToProc)
 MDEALLOCATE(FIBGMToProc_Shared)
 MDEALLOCATE(FIBGMProcs)
 MDEALLOCATE(FIBGMProcs_Shared)
-MDEALLOCATE(CNTotalElem2GlobalElem)
-MDEALLOCATE(CNTotalElem2GlobalElem_Shared)
-MDEALLOCATE(CNTotalSide2GlobalSide)
-MDEALLOCATE(CNTotalSide2GlobalSide_Shared)
+IF (nComputeNodeProcessors.NE.nProcessors_Global) THEN
+  MDEALLOCATE(CNTotalElem2GlobalElem)
+  MDEALLOCATE(CNTotalElem2GlobalElem_Shared)
+  MDEALLOCATE(CNTotalSide2GlobalSide)
+  MDEALLOCATE(CNTotalSide2GlobalSide_Shared)
+END IF ! nComputeNodeProcessors.NE.nProcessors_Global
 
 #if USE_MPI
 CALL FinalizeHaloInfo()

piclas-framework/piclas

src/particles/particle_mesh/particle_bgm.f90
@@ -1486,14 +1486,17 @@ CALL UNLOCK_AND_FREE(FIBGM_offsetElem_Shared_Win)
 CALL UNLOCK_AND_FREE(FIBGM_Element_Shared_Win)
 CALL UNLOCK_AND_FREE(FIBGMToProc_Shared_Win)
 CALL UNLOCK_AND_FREE(FIBGMProcs_Shared_Win)
-CALL UNLOCK_AND_FREE(GlobalElem2CNTotalElem_Shared_Win)
-CALL UNLOCK_AND_FREE(GlobalSide2CNTotalSide_Shared_Win)
-CALL UNLOCK_AND_FREE(CNTotalElem2GlobalElem_Shared_Win)
-CALL UNLOCK_AND_FREE(CNTotalSide2GlobalSide_Shared_Win)
+IF (nComputeNodeProcessors.NE.nProcessors_Global) THEN
+  CALL UNLOCK_AND_FREE(GlobalElem2CNTotalElem_Shared_Win)
+  CALL UNLOCK_AND_FREE(GlobalSide2CNTotalSide_Shared_Win)
+  CALL UNLOCK_AND_FREE(CNTotalElem2GlobalElem_Shared_Win)
+  CALL UNLOCK_AND_FREE(CNTotalSide2GlobalSide_Shared_Win)
+END IF ! nComputeNodeProcessors.NE.nProcessors_Global
 
 CALL MPI_BARRIER(MPI_COMM_SHARED,iERROR)
 
 ! Then, free the pointers or arrays
+<<<<<<< HEAD:src/particles/particle_mesh/particle_bgm.f90
 SDEALLOCATE(CNTotalElem2GlobalElem)
 SDEALLOCATE(CNTotalSide2GlobalSide)
 SDEALLOCATE(GlobalSide2CNTotalSide)
@@ -1513,14 +1516,16 @@ ADEALLOCATE(FIBGMToProc)
 ADEALLOCATE(FIBGMToProc_Shared)
 ADEALLOCATE(FIBGMProcs)
 ADEALLOCATE(FIBGMProcs_Shared)
-ADEALLOCATE(GlobalElem2CNTotalElem)
-ADEALLOCATE(GlobalElem2CNTotalElem_Shared)
-ADEALLOCATE(GlobalSide2CNTotalSide)
-ADEALLOCATE(GlobalSide2CNTotalSide_Shared)
-ADEALLOCATE(CNTotalElem2GlobalElem)
-ADEALLOCATE(CNTotalElem2GlobalElem_Shared)
-ADEALLOCATE(CNTotalSide2GlobalSide)
-ADEALLOCATE(CNTotalSide2GlobalSide_Shared)
+IF (nComputeNodeProcessors.NE.nProcessors_Global) THEN
+  ADEALLOCATE(GlobalElem2CNTotalElem)
+  ADEALLOCATE(GlobalElem2CNTotalElem_Shared)
+  ADEALLOCATE(GlobalSide2CNTotalSide)
+  ADEALLOCATE(GlobalSide2CNTotalSide_Shared)
+  ADEALLOCATE(CNTotalElem2GlobalElem)
+  ADEALLOCATE(CNTotalElem2GlobalElem_Shared)
+  ADEALLOCATE(CNTotalSide2GlobalSide)
+  ADEALLOCATE(CNTotalSide2GlobalSide_Shared)
+END IF ! nComputeNodeProcessors.NE.nProcessors_Global
 
 #if USE_MPI
 CALL FinalizeHaloInfo()