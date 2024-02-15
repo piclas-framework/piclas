777a2b966ea4068b60ae0da5e87bf9dfda48a839
Stephen Copplestone <stephen@copplestone.de>
2024-02-15 16:18:30 +0100

    prevent lost particles when doing a virtual shift

--- src/particles/particle_operations.f90
+++ src/particles/particle_operations.f90
@@ -160,6 +160,7 @@ USE MOD_HDG_Vars                  ,ONLY: UseFPC,FPC,UseEPC,EPC
 USE MOD_Mesh_Vars                 ,ONLY: BoundaryType
 #endif /*USE_HDG*/
 USE MOD_Particle_Vars             ,ONLY: PartState, LastPartPos
+!USE MOD_Particle_Mesh_Vars        ,ONLY: GEO
 !----------------------------------------------------------------------------------------------------------------------------------!
 IMPLICIT NONE
 ! INPUT / OUTPUT VARIABLES
@@ -170,7 +171,7 @@ LOGICAL, INTENT(OUT),OPTIONAL :: crossedBC               !< optional flag is nee
 !----------------------------------------------------------------------------------------------------------------------------------!
 ! LOCAL VARIABLES
 INTEGER                       :: iSpec, iSF
-REAL                          :: MPF
+REAL                          :: MPF!,RandVal(2)
 #if USE_HDG
 INTEGER                       :: iBC,iUniqueFPCBC,iUniqueEPCBC,BCState
 #endif /*USE_HDG*/
@@ -300,14 +301,18 @@ IF(PRESENT(BCID)) THEN
   END IF ! UseEPC
 #endif /*USE_HDG*/
 
-  ! Debugging: Move particles that impact the left BC to a specific location
-  !IF(TRIM(BoundaryName(PartBound%MapToFieldBC(BCID))).EQ.'BC_LEFT')THEN
-  !  PartSpecies(PartID)        = 3
-  !  PDM%ParticleInside(PartID) = .TRUE.
-  !  PartState(1,PartID)        = -9e-9 ! (/-9e-9, 0., 0./)
-  !  LastPartPos(1,PartID)      = -9e-9 ! (/-9e-9, 0., 0./)
-  !  PartState(4:6,PartID)      = (/0., 0., 0./)
-  !END IF ! TRIM(BoundaryName(PartBound%MapToFieldBC(BCID))).EQ.'BC_LEFT'
+   ! ! Debugging: Move particles that impact the left BC to a specific location
+   ! IF(TRIM(BoundaryName(PartBound%MapToFieldBC(BCID))).EQ.'BC_LEFT')THEN
+   !   PartSpecies(PartID)        = 3
+   !   PDM%ParticleInside(PartID) = .TRUE.
+   !   PartState(1,PartID)        = -9e-9
+   !   LastPartPos(1,PartID)      = PartState(1,PartID)
+   !   CALL RANDOM_NUMBER(RandVal)
+   !   PartState(2,PartID)        = RandVal(1)*(GEO%zmaxglob-GEO%zminglob) + GEO%zminglob
+   !   PartState(3,PartID)        = RandVal(2)*(GEO%ymaxglob-GEO%yminglob) + GEO%yminglob
+   !   LastPartPos(2:3,PartID)    = PartState(2:3,PartID)
+   !   PartState(4:6,PartID)      = (/0., 0., 0./)
+   ! END IF ! TRIM(BoundaryName(PartBound%MapToFieldBC(BCID))).EQ.'BC_LEFT'
 END IF ! PRESENT(BCID)
 
 ! Tracking-relevant variables (not required if a particle is removed within the domain, e.g. removal due to radial weighting)
