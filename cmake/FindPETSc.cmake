83eac18f863d4cea0f629272840672980758fe13
Paul Nizenkov <p.nizenkov@gmail.com>
2024-06-25 19:28:03 +0200

    Changed variables names to PETSC_, fixed build version variable

--- cmake/FindPETSc.cmake
+++ cmake/FindPETSc.cmake
@@ -30,19 +30,19 @@
 #
 # This module will set the following variables in your project:
 #
-#  PETSc_FOUND          - if false, do not try to link to PETSc
-#  PETSc_LIBRARIES      - a list of the full paths to all libraries
-#  PETSc_INCLUDE_DIRS   - a list of all include directories
-#  PETSc_VERSION        - the full version of PETSc MAJOR.MINOR.PATCH
-#  PETSc_VERSION_MAJOR  - the MAJOR part of PETSc_VERSION
-#  PETSc_VERSION_MINOR  - the MINOR part of PETSc_VERSION
-#  PETSc_VERSION_PATCH  - the PATCH part of PETSc_VERSION
+#  PETSC_FOUND          - if false, do not try to link to PETSc
+#  PETSC_LIBRARIES      - a list of the full paths to all libraries
+#  PETSC_INCLUDE_DIRS   - a list of all include directories
+#  PETSC_VERSION        - the full version of PETSc MAJOR.MINOR.PATCH
+#  PETSC_VERSION_MAJOR  - the MAJOR part of PETSC_VERSION
+#  PETSC_VERSION_MINOR  - the MINOR part of PETSC_VERSION
+#  PETSC_VERSION_PATCH  - the PATCH part of PETSC_VERSION
 #
 # Author: Frédéric Simonis @fsimonis
 
 cmake_policy(VERSION 3.10)
 
-# Generate a argument for cmake pkg-config call
+# Generate a argument for cmake pkg-config call (Note: variable uses PETSc spelling due to FIND_PACKAGE call in CMakelistsLib.txt)
 if(PETSc_FIND_QUIETLY)
   find_package(PkgConfig QUIET)
 else()
@@ -52,11 +52,11 @@ endif()
 if(PKG_CONFIG_FOUND)
   # Build the pkg-config version spec
   set(_pkg_version_spec "")
-  if(DEFINED PETSc_FIND_VERSION)
-    if(PETSc_FIND_VERSION_EXACT)
-      set(_pkg_version_spec "=${PETSc_FIND_VERSION}")
+  if(DEFINED PETSC_FIND_VERSION)
+    if(PETSC_FIND_VERSION_EXACT)
+      set(_pkg_version_spec "=${PETSC_FIND_VERSION}")
     else()
-      set(_pkg_version_spec ">=${PETSc_FIND_VERSION}")
+      set(_pkg_version_spec ">=${PETSC_FIND_VERSION}")
     endif()
   endif()
 
@@ -68,25 +68,25 @@ if(PKG_CONFIG_FOUND)
   set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH "YES")
 
   if(PETSc_FIND_QUIETLY)
-    pkg_check_modules(PETSc QUIET IMPORTED_TARGET GLOBAL "PETSc${_pkg_version_spec}")
+    pkg_check_modules(PETSC QUIET IMPORTED_TARGET GLOBAL "petsc${_pkg_version_spec}")
   else()
-    pkg_check_modules(PETSc       IMPORTED_TARGET GLOBAL "PETSc${_pkg_version_spec}")
+    pkg_check_modules(PETSC       IMPORTED_TARGET GLOBAL "petsc${_pkg_version_spec}")
   endif()
 
   unset(_pkg_version_spec)
 
   # Extract version parts from the version information
-  if(PETSc_VERSION)
+  if(PETSC_VERSION)
     set(_petsc_versions "")
-    string(REGEX MATCHALL "[0-9]+" _petsc_versions ${PETSc_VERSION})
+    string(REGEX MATCHALL "[0-9]+" _petsc_versions ${PETSC_VERSION})
     list(GET _petsc_versions 0 _petsc_version_major)
     list(GET _petsc_versions 1 _petsc_version_minor)
     list(GET _petsc_versions 2 _petsc_version_patch)
 
-    set(PETSc_VERSION ${PETSc_VERSION} CACHE STRING "Full version of PETSc")
-    set(PETSc_VERSION_MAJOR ${_petsc_version_major} CACHE INTERNAL "Major version of PETSc")
-    set(PETSc_VERSION_MINOR ${_petsc_version_minor} CACHE INTERNAL "Minor version of PETSc")
-    set(PETSc_VERSION_PATCH ${_petsc_version_patch} CACHE INTERNAL "Patch version of PETSc")
+    set(PETSC_VERSION ${PETSC_VERSION} CACHE STRING "Full version of PETSc")
+    set(PETSC_VERSION_MAJOR ${_petsc_version_major} CACHE INTERNAL "Major version of PETSc")
+    set(PETSC_VERSION_MINOR ${_petsc_version_minor} CACHE INTERNAL "Minor version of PETSc")
+    set(PETSC_VERSION_PATCH ${_petsc_version_patch} CACHE INTERNAL "Patch version of PETSc")
 
     unset(_petsc_versions)
     unset(_petsc_version_major)
@@ -98,14 +98,14 @@ unset(_petsc_quiet_arg)
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (PETSc
-  REQUIRED_VARS PETSc_FOUND PETSc_INCLUDE_DIRS PETSc_LIBRARIES PETSc_LINK_LIBRARIES
-  VERSION_VAR PETSc_VERSION
+  REQUIRED_VARS PETSC_FOUND PETSC_INCLUDE_DIRS PETSC_LIBRARIES PETSC_LINK_LIBRARIES
+  VERSION_VAR PETSC_VERSION
   )
 
-if(PETSc_FOUND)
+if(PETSC_FOUND)
   if(NOT TARGET PETSc::PETSc)
-    add_library(PETSc::PETSc ALIAS PkgConfig::PETSc)
+    add_library(PETSc::PETSc ALIAS PkgConfig::PETSC)
   endif()
 endif()
 
-mark_as_advanced(PETSc_INCLUDE_DIRS PETSc_LIBRARIES PETSc_VERSION_MAJOR PETSc_VERSION_MINOR PETSc_VERSION_PATCH VERSION_VAR PETSc_VERSION)
+mark_as_advanced(PETSC_INCLUDE_DIRS PETSC_LIBRARIES PETSC_VERSION_MAJOR PETSC_VERSION_MINOR PETSC_VERSION_PATCH VERSION_VAR PETSC_VERSION)
