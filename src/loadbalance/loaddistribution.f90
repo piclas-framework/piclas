c573395a49775fdaf87ef2d9b469b06247fb89dc
Stephen Copplestone <stephen@copplestone.de>
2025-01-15 15:03:27 +0100

    fixed ElemGlobalTime in load distribution from INTENT(IN) to INTENT(INOUT) as it is used directly in MPI_BCAST

--- src/loadbalance/loaddistribution.f90
+++ src/loadbalance/loaddistribution.f90
@@ -581,7 +581,7 @@ IMPLICIT NONE
 ! INPUT VARIABLES
 INTEGER,INTENT(IN)             :: nProcs
 INTEGER,INTENT(IN)             :: nGlobalElems
-REAL,INTENT(IN)                :: ElemGlobalTime(:)
+REAL,INTENT(INOUT)             :: ElemGlobalTime(:)
 LOGICAL,INTENT(IN)             :: ElemTimeExists
 INTEGER,INTENT(IN)             :: PartsInElem(:)
 !-----------------------------------------------------------------------------------------------------------------------------------
