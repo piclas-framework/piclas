5105284f5938f715dcf9be7603d1f5fa44e15803
Tobias Ott <ottt@irs.uni-stuttgart.de>
2025-02-19 17:35:23 +0100

    removed LambdaMax in Surf_N_Type

--- src/hdg/hdg_vars.f90
+++ src/hdg/hdg_vars.f90
@@ -49,9 +49,7 @@ TYPE(HDG_Vol_N_Type),ALLOCATABLE :: HDG_Vol_N(:)      !<
 
 ! HDG side variables
 TYPE HDG_Surf_N_Type
-! TODO NSideMin - LambdaMax: Rename it so that it actually makes sense
   REAL,ALLOCATABLE    :: lambda(:,:)          !< lambda, ((NSideMin+1)^2,nSides) where NSideMin is the minimum of the two faces
-  REAL,ALLOCATABLE    :: lambdaMax(:,:)       !< lambda, ((NSideMax+1)^2,nSides) where NSideMax is the maximum of the two faces
   REAL,ALLOCATABLE    :: Precond(:,:)         !< block diagonal preconditioner for lambda(nGP_face, nGP-face, nSides)
   REAL,ALLOCATABLE    :: InvPrecondDiag(:)    !< 1/diagonal of Precond
   REAL,ALLOCATABLE    :: qn_face(:,:)         !< for Neumann BC
