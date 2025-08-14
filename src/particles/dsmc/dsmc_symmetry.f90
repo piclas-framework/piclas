c8198b49925562f1e8b38941c8b8a5a22bc18b04
Kim Ellenberger <ellenbergerk@irs.uni-stuttgart.de>
2025-08-14 11:42:53 +0200

    bugfix clone in 2D tracking

--- src/particles/dsmc/dsmc_symmetry.f90
+++ src/particles/dsmc/dsmc_symmetry.f90
@@ -267,7 +267,7 @@ DO iPart = 1, ParticleWeighting%ClonePartNum(DelayCounter)
   PartSpecies(PositionNbr) = ClonedParticles(iPart,DelayCounter)%Species
   ! Set the global element number with the offset
   PEM%GlobalElemID(PositionNbr) = ClonedParticles(iPart,DelayCounter)%Element
-  PEM%LastGlobalElemID(PositionNbr) = PEM%GlobalElemID(PositionNbr)
+  PEM%LastGlobalElemID(PositionNbr) = 0
   locElemID = PEM%LocalElemID(PositionNbr)
   LastPartPos(1:3,PositionNbr) = PartState(1:3,PositionNbr)
   PartMPF(PositionNbr) =  ClonedParticles(iPart,DelayCounter)%WeightingFactor
